62bdae0adde7808adccd570c676e9caae673f371
Lingcao Huang <huanglingcao@gmail.com>
2018-03-21 15:45:35 +0800

    add convert to 16 bit

--- convert_to_8bit.sh
+++ convert_to_8bit.sh
@@ -20,6 +20,25 @@ function convert_to8bit() {
     gdal_translate -ot Byte -scale ${minValue} ${maxValue} 0 255 ${input} ${output}
 }
 
+# function of converting to 8bit using gdal_translate with max and min value.
+function convert_to16bit() {
+    local minValue=${1}
+    local maxValue=${2}
+
+    local input=${3}
+
+     # get file name without extension
+    DIR=$(dirname "${input}")
+    filename=$(basename "$input")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+
+    output=${DIR}/${filename}_16bit.png
+
+    gdal_translate -of PNG -ot Int16 -scale ${minValue} ${maxValue} 0 255 ${input} ${output}
+}
+
+
 # run this in ~/Data/2018_IEEE_GRSS_Data_Fusion/2018_Release_Phase1/Lidar_GeoTiff_Rasters
 
 cd ~/Data/2018_IEEE_GRSS_Data_Fusion/2018_Release_Phase1/Lidar_GeoTiff_Rasters
@@ -33,6 +52,14 @@ convert_to8bit 210 3845 Intensity_C1/UH17_GI1F051_TR.tif
 convert_to8bit 207 5174 Intensity_C2/UH17_GI2F051_TR.tif
 
 convert_to8bit 144 8583 Intensity_C3/UH17_GI3F051_TR.tif
+
+
+
+convert_to16bit 210 3845 Intensity_C1/UH17_GI1F051_TR.tif
+
+convert_to16bit 207 5174 Intensity_C2/UH17_GI2F051_TR.tif
+
+convert_to16bit 144 8583 Intensity_C3/UH17_GI3F051_TR.tif
 
 
 
