7ab2f05a593f47f4c9c7d96d0604a5ff39b467cd
yghlc <hlchuang@126.com>
2018-05-05 22:01:39 +0800

    can run and output results

--- image_classify_rnn_tf.py
+++ image_classify_rnn_tf.py
@@ -28,14 +28,19 @@ session = tf.Session(config=config)
 
 
 import matplotlib.pyplot as plt
+import datetime
+import time
+
+import sklearn
 
 #we only have 20 classes, however, class index start from 1, so use 21. We should ignore the "0" class in the end
 num_classes = 21
-
+learn_rate = 0.001
 hidden_units = 128
 batch_size = 2048
+lstm_layer_num = 5
 
-epochs = 200
+epoches = 200
 
 def read_oneband_image_to_1dArray(image_path):
 
@@ -132,9 +137,30 @@ def split_data(x_all, y_all, test_percent=0.01):
 #
 #     return model
 
+def lstm_layer(hidden_units,keep_prob):
+    """
+    setting a lstm layer
+    :param hidden_units: the number of hidden units in this layer
+    :param keep_prob: the dropout parameters, i.e. how many percent want to keep
+    :return: this layer
+    """
 
+    layer = tf.contrib.rnn.BasicLSTMCell(hidden_units)
+    # layer = tf.contrib.rnn.LSTMCell(hidden_units)
+
+    # dropout, only apply to the output units
+    cell = tf.contrib.rnn.DropoutWrapper(layer, output_keep_prob=keep_prob)
+    return layer
+
+def multi_lstm_layers(hidden_units,keep_prob,layer_num):
+
+    mlstm_cell = tf.contrib.rnn.MultiRNNCell([lstm_layer(hidden_units, keep_prob) for _ in range(layer_num)],
+                                             state_is_tuple=True)
+    return mlstm_cell
 
 def main(options, args):
+
+    t0 = time.time()
 
     label_image = args[0]
     multi_spec_image_path = args[1]
@@ -175,6 +201,9 @@ def main(options, args):
     print(x_train.shape[0],y_train.shape[0], 'train samples')
     print(x_test.shape[0],x_test.shape[0], 'test samples')
 
+    train_samples_num = x_train.shape[0]
+    test_samples_num = x_test.shape[0]
+
     x_train = x_train.astype('float32')
     x_test = x_test.astype('float32')
 
@@ -182,38 +211,132 @@ def main(options, args):
     x_train /= 65536
     x_test /= 65536
 
-    bands = x_train.shape[1:]
+    # bands is the same as feature number of this multi-spectral images
+    bands = list(x_train.shape[1:])
+    band_num = x_train.shape[1]
 
     ## start tensorflow codes here
+    X_input = tf.placeholder(tf.float32, [None, band_num])
+    X_input = tf.reshape(X_input,[-1,band_num,1])
+    y_input = tf.placeholder(tf.float32, [None, num_classes])
 
+    # batch size can be changed during the last step in one epoch
+    batch_size_v = tf.placeholder(tf.int32, [])
+    keep_prob_v = tf.placeholder(tf.float32, [])   # keep_prob_v is different when training and prediction
 
+    mulit_lstm = multi_lstm_layers(hidden_units,keep_prob_v,lstm_layer_num)
 
+    init_state = mulit_lstm.zero_state(batch_size_v, dtype=tf.float32)
+    outputs = []
+    state = init_state
+    with tf.variable_scope('RNN'):
+        for step in range(band_num):
+            (cell_output, state) = mulit_lstm(X_input[:, step,:], state)
+            outputs.append(cell_output)
+    h_state = outputs[-1]
 
+    # weight and bias
+    W = tf.Variable(tf.truncated_normal([hidden_units, num_classes], stddev=0.1), dtype=tf.float32)
+    bias = tf.Variable(tf.constant(0.1, shape=[num_classes]), dtype=tf.float32)
+    y_pre = tf.nn.softmax(tf.matmul(h_state, W) + bias)
 
+    # loss and evaluation
+    # cross_entropy = -tf.reduce_mean(y_input*tf.log(y_pre))
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_pre,labels=y_input))
+    train_op = tf.train.AdadeltaOptimizer(learning_rate=learn_rate).minimize(cross_entropy)
+
+    correct_pre = tf.equal(tf.argmax(y_pre,1),tf.argmax(y_input,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pre,"float"))
+
+    session.run(tf.global_variables_initializer())
+
+    history = {'loss':[],'val_loss':[],'acc':[],'val_acc':[]}
+
+    datetime_str = datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
+    f_obj = open("%s_loss.txt"%datetime_str,"w")
+
+    for epoch in range(epoches):
+        t_epoch = time.time()
+        train_loss = 0.0
+        train_acc = 0.0
+        val_loss = 0.0
+        val_acc = 0.0
+
+        #shuffle train data
+        x_train,y_train = sklearn.utils.shuffle(x_train,y_train)
+
+        batch_num = train_samples_num/batch_size
+        batch_last_size = train_samples_num%batch_size
+        if batch_last_size>0:
+            batch_num += 1
+        # train
+        for iter in range(batch_num):
+            temp_batch_size = batch_size
+            if iter== batch_num-1:
+                temp_batch_size = batch_last_size
+                X_batch = x_train[iter*batch_size:]
+                y_batch = y_train[iter*batch_size:]
+                # print(X_batch.shape,y_batch.shape)
+            else:
+                X_batch = x_train[iter*batch_size:(iter+1)*batch_size]
+                y_batch = y_train[iter*batch_size:(iter+1)*batch_size]
+
+            cost, acc, _ = session.run([cross_entropy, accuracy, train_op],
+                                feed_dict={X_input: X_batch, y_input: y_batch, keep_prob_v: 0.5, batch_size_v: temp_batch_size})
+            train_loss += cost
+            train_acc += acc
+            print("iter {}, train loss={:.6f}, acc={:.6f}".format(iter+1,cost,acc))
+
+        train_loss /= batch_num
+        train_acc /= batch_num
+        # test
+        X_batch, y_batch = x_test,y_test
+
+        _cost, _acc = session.run([cross_entropy, accuracy],
+                                feed_dict={X_input: X_batch, y_input: y_batch, keep_prob_v: 1.0,
+                                                  batch_size_v: test_samples_num})
+        val_acc = _acc
+        val_loss = _cost
+        out_str = "epoch {}, train loss={:.6f}, acc={:.6f}; test loss={:.6f}, acc={:.6f}; time cost: {:.2f} seconds".\
+              format(epoch + 1, train_loss, train_acc, val_loss,val_acc, time.time() - t_epoch)
+        print(out_str)
+        f_obj.writelines(out_str+'\n')
+
+        history['acc'].append(train_acc)
+        history['val_acc'].append(val_acc)
+        history['loss'].append(train_loss)
+        history['val_loss'].append(val_loss)
+
+    f_obj.close()
 
     # list all data in history
-    print(history.history.keys())
+    print(history.keys())
+
     # summarize history for accuracy
     plt.figure(0)
-    plt.plot(history.history['acc'])
-    plt.plot(history.history['val_acc'])
+    plt.plot(history['acc'])
+    plt.plot(history['val_acc'])
     plt.title('model accuracy')
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
     # plt.show()
-    plt.savefig('acc_his_%d.png'%random.randint(1,1000),dpi=300)
+    plt.savefig('%s_acc_his_%d.png'%(datetime_str,random.randint(1,1000)),dpi=300)
     # summarize history for loss
     plt.figure(1)
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
+    plt.plot(history['loss'])
+    plt.plot(history['val_loss'])
     plt.title('model loss')
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
     # plt.show()
-    plt.savefig('loss_his_%d.png'%random.randint(1,1000),dpi=300)
+    plt.savefig('%s_loss_his_%d.png'%(datetime_str,random.randint(1,1000)),dpi=300)
 
+
+    t1 = time.time()
+    total = t1 - t0
+    print('complete, total time cost: %.2f seconds or %.2f minutes or %.2f hours' % (total,total/60.0,total/3600.0))
 
 
 
