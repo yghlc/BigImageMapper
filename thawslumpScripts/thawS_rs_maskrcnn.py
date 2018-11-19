b2a6c3c3553f7ba435b1573be8ae40b2895cdfe8
yghlc <hlchuang@126.com>
2018-11-19 10:39:04 -0700

    check number of class when loading mask

--- thawslumpScripts/thawS_rs_maskrcnn.py
+++ thawslumpScripts/thawS_rs_maskrcnn.py
@@ -188,6 +188,9 @@ class PlanetDataset(utils.Dataset):
 
         height, width = label.shape
         unique_ids, counts = np.unique(label, return_counts=True)
+        if len(unique_ids) != PlanetConfig.NUM_CLASSES:
+            raise ValueError(str(unique_ids)+'its count is: %d but number of classes is: %d'
+                             %(len(unique_ids),PlanetConfig.NUM_CLASSES))
 
         # create the mask for each class (excluding the background)
         for id, count in zip(unique_ids,counts):
