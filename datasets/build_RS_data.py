7a69191e5c5c5d4825a9bc79b3e2db8b4c5b070d
yghlc <hlchuang@126.com>
2018-11-21 12:02:06 +0800

    python3

--- datasets/build_RS_data.py
+++ datasets/build_RS_data.py
@@ -131,12 +131,13 @@ def save_patch_oneband_8bit(patch_obj,img_data,save_path):
 
     profile.update(dtype=rasterio.uint8, count=1, transform=new_transform, width=xsize, height=ysize)
     # set the block size, it should be a multiple of 16 (TileLength must be a multiple of 16)
-    if profile.has_key('blockxsize') and profile['blockxsize'] > xsize:
+    # if profile.has_key('blockxsize') and profile['blockxsize'] > xsize:
+    if 'blockxsize' in profile and profile['blockxsize'] > xsize: # python3 & 2
         if xsize % 16 == 0:
             profile.update(blockxsize=xsize)
         else:
             profile.update(blockxsize=16)  # profile.update(blockxsize=16)
-    if profile.has_key('blockysize') and profile['blockysize'] > ysize:
+    if 'blockysize' in profile and profile['blockysize'] > ysize:
         if ysize % 16 == 0:
             profile.update(blockysize=ysize)
         else:
