2abac3e053359922d6a42b589c288745caf6f0c9
Lingcao Huang <huanglingcao@gmail.com>
2018-12-08 18:58:20 -0700

    don't be quiet

--- planetScripts/crop_beiluhe_basin.sh
+++ planetScripts/crop_beiluhe_basin.sh
@@ -2,5 +2,5 @@
 
 name_pre=20180522_035755_3B_AnalyticMS_SR_mosaic
 
-gdalwarp -q -cutline ~/Data/Qinghai-Tibet/beiluhe/beiluhe_reiver_basin.kml \
+gdalwarp -cutline ~/Data/Qinghai-Tibet/beiluhe/beiluhe_reiver_basin.kml \
 -crop_to_cutline -tr 3.0 3.0 -of GTiff ${name_pre}.tif ${name_pre}_basinExt.tif
