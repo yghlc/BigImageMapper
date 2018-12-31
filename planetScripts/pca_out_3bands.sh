ad7a5832eb54dc79c246d7e96fccae2d9edeb71d
Lingcao Huang <huanglingcao@gmail.com>
2018-12-30 20:10:20 -0700

    debug

--- planetScripts/pca_out_3bands.sh
+++ planetScripts/pca_out_3bands.sh
@@ -6,18 +6,26 @@
 # test
 cd ~/Data/Qinghai-Tibet/beiluhe/beiluhe_planet/beiluhe_basin/201805
 
-name_pre=20180522_035755_3B_AnalyticMS_SR_mosaic
-
+#name_pre=20180522_035755_3B_AnalyticMS_SR_mosaic
+name_pre=20180522_035755_3B_AnalyticMS_SR_mosaic_subPCA
 # Exit immediately if a command exits with a non-zero status. E: error trace
 set -eE -o functrace
 
 keep_bands=3
-out_min=0
-out_max=255
-b_norma=1  # 1 for on, 0 for off
+out_min=10.0
+out_max=200.0
+b_norma=0  # 1 for on, 0 for off
 pixel=uint8 #pixel=uint8/uint16/int16/uint32/int32/float/double] (default value is float)
 
-otbcli_DimensionalityReduction -progress 1 -in ${name_pre}_8bit.tif -out ${name_pre}_8bit_PCA3b.tif ${pixel}  -method pca \
--nbcomp ${keep_bands} -rescale.outmin ${out_min} -rescale.outmax ${out_max} -normalize ${b_norma}
+#${pixel}
+
+# -nbcomp ${keep_bands}
+
+otbcli_DimensionalityReduction -progress 1 -in ${name_pre}_8bit.tif -out ${name_pre}_8bit_PCA3b.tif   -method pca \
+-normalize ${b_norma}  -rescale.outmin ${out_min} -rescale.outmax ${out_max} \
+-outxml ${name_pre}_save.xml
+
+#  -rescale ${out_min} ${out_max}
+# -rescale.outmin ${out_min} -rescale.outmax ${out_max}
 
 
