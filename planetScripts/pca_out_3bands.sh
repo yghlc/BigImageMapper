2eeae74424481aeb8af29202600f5b71c3f03914
Lingcao Huang <huanglingcao@gmail.com>
2019-01-01 08:16:19 -0700

    allow input file name

--- planetScripts/pca_out_3bands.sh
+++ planetScripts/pca_out_3bands.sh
@@ -20,7 +20,7 @@ b_norma=1  # 1 for on, 0 for off
 # seems min and max option not work, we cannot set this one as int type
 pixel=float #pixel=uint8/uint16/int16/uint32/int32/float/double] (default value is float)
 
-otbcli_DimensionalityReduction -progress 1 -in ${name_pre}_8bit.tif -out ${name_pre}_8bit_PCA3b.tif ${pixel} -method pca \
+otbcli_DimensionalityReduction -progress 1 -in ${name_pre}.tif -out ${name_pre}_PCA3b.tif ${pixel} -method pca \
 -normalize ${b_norma} -rescale.outmin ${out_min} -rescale.outmax ${out_max} \
 -nbcomp ${keep_bands} \
 -outxml ${name_pre}_save.xml
@@ -29,7 +29,7 @@ otbcli_DimensionalityReduction -progress 1 -in ${name_pre}_8bit.tif -out ${name_
 # -rescale.outmin ${out_min} -rescale.outmax ${out_max}
 
 #convert to 8 bit, 0-255
-gdal_contrast_stretch -percentile-range 0.01 0.99 ${name_pre}_8bit_PCA3b.tif ${name_pre}_8bit_PCA3b_8bit.tif
+gdal_contrast_stretch -percentile-range 0.01 0.99 ${name_pre}_PCA3b.tif ${name_pre}_PCA3b_8bit.tif
 
 
 
