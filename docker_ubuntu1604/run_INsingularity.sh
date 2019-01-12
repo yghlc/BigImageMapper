e110e10fbd5c8178353d59a5014048f6e283fe19
Lingcao Huang <huanglingcao@gmail.com>
2019-01-11 21:51:25 -0700

    nvidia support

--- docker_ubuntu1604/run_INsingularity.sh
+++ docker_ubuntu1604/run_INsingularity.sh
@@ -8,7 +8,7 @@ sing_img=~/codes/PycharmProjects/Landuse_DL/docker_ubuntu1604/ubuntu16.04_itsc_t
 export SINGULARITY_BINDPATH=/500G:/500G,/DATA1:/DATA1
 
 SINGULARITYENV_PATH=$PATH SINGULARITYENV_LD_LIBRARY_PATH=$LD_LIBRARY_PATH \
-singularity exec ${sing_img} ${exe_script}
+singularity exec --nv ${sing_img} ${exe_script}
 
 
 
