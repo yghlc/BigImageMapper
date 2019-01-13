99bf5a6450a587f0497c240c321a5d99a30e1b17
Lingcao Huang <huanglingcao@gmail.com>
2019-01-13 12:50:35 -0700

    add /bin to $PATH

--- docker_ubuntu1604/run_INsingularity.sh
+++ docker_ubuntu1604/run_INsingularity.sh
@@ -7,7 +7,12 @@ sing_img=~/codes/PycharmProjects/Landuse_DL/docker_ubuntu1604/ubuntu16.04_itsc_t
 # set mount disk on Cryo03
 export SINGULARITY_BINDPATH=/500G:/500G,/DATA1:/DATA1
 
+# on cryo03
 SINGULARITYENV_PATH=$PATH SINGULARITYENV_LD_LIBRARY_PATH=$LD_LIBRARY_PATH \
+singularity exec --nv ${sing_img} ${exe_script}
+
+# on itsc service
+SINGULARITYENV_PATH=/bin:$PATH SINGULARITYENV_LD_LIBRARY_PATH=$LD_LIBRARY_PATH \
 singularity exec --nv ${sing_img} ${exe_script}
 
 
