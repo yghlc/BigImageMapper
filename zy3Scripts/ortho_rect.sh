a9932160098c070e3b6cabcfd26f605fd6d63426
Lingcao Huang <huanglingcao@gmail.com>
2019-01-18 16:51:08 -0700

    add comments

--- zy3Scripts/ortho_rect.sh
+++ zy3Scripts/ortho_rect.sh
@@ -7,7 +7,7 @@
 #export PATH=~/programs/StereoPipeline-2.6.1-2018-09-06-x86_64-OSX/bin:$PATH
 export PATH=~/programs/StereoPipeline-2.6.1-2018-09-06-x86_64-Linux/bin:$PATH
 
-#number of thread of to use, 8 or 16 on linux, 4 on mac
+#number of thread of to use, 8 or 16 on linux, 4 on mac (default is 4)
 num_thr=8
 
 #dem=~/Data/Qinghai-Tibet/beiluhe/DEM/srtm_30/beiluhe_strm30.tif
@@ -21,6 +21,7 @@ output=${prename}_prj.tif
 
 # ${prename}.xml
 # sicne ASP complain the xml is not recognised, then remove it. the script still work without this complaint
+# set METADATATYPE=ZY3 (original is DG), then mapproject would not copy the rpb files
 mapproject -t rpc $dem ${prename}.tiff  ${output} --mpp ${our_res} \
 	--ot UInt16 --tif-compress None --mo METADATATYPE=ZY3 --threads ${num_thr}
 
