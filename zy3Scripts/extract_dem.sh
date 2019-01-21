84d1fb80255e6f65f951c97d4869a5d0fe98c27c
Lingcao Huang <huanglingcao@gmail.com>
2019-01-21 09:49:31 -0700

    copy rpc file on Linux, no compression, Uint16

--- zy3Scripts/extract_dem.sh
+++ zy3Scripts/extract_dem.sh
@@ -41,17 +41,21 @@ test_roi=
 # but rather uses an existing terrain model as an initial guess, and improves on it.
 
 mapproject -t rpc --tr ${out_res} ${dem} ${left} left_mapped.tif \
-        ${test_roi} --threads ${num_thr}
+        ${test_roi} --threads ${num_thr} --ot UInt16 --tif-compress None
 
 mapproject -t rpc --tr ${out_res} ${dem} ${right} right_mapped.tif \
-        ${test_roi} --threads ${num_thr}
+        ${test_roi} --threads ${num_thr} --ot UInt16 --tif-compress None
 #      --tr 0.5 srtm_53_07.tif                            \
 #      12FEB12053341-P1BS_R2C1-052783824050_01_P001.TIF   \
 #      12FEB12053341-P1BS_R2C1-052783824050_01_P001.XML   \
 #      right_mapped.tif
 
-stereo -t rpcmaprpc --subpixel-mode 3 --alignment-method none  \
-           left_mapped.tif right_mapped.tif                 \
+# copy rpc file on Linux
+cp "${left%.*}".rpb left_mapped.RPB
+cp "${right%.*}".rpb right_mapped.RPB
+
+stereo -t rpcmaprpc --subpixel-mode 3 --alignment-method none     \
+           left_mapped.tif right_mapped.tif --threads ${num_thr}    \
            ${output} ${dem}
 
 
@@ -69,7 +73,6 @@ point2dem  --search-radius-factor 10 --nodata-value -9999 --tr ${dem_res} ${outp
 # one could change in point2dem the search radius factor, use hole-􏰟filling,
 # invoke more aggressive outlier removal, and erode pixels at the boundary
 # (those tend to be less reliable). Alternatively, holes can be filled with dem_mosaic.
-
 
 
 # Creating DEMs Relative to the Geoid/Areoid, after this the dem is close the SRTM (difference < a few meters)
