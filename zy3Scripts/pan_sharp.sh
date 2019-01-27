dc5f43b9182f9f5c61064985da0f4de226aea793
Lingcao Huang <huanglingcao@gmail.com>
2019-01-27 15:57:49 -0700

    rgb

--- zy3Scripts/pan_sharp.sh
+++ zy3Scripts/pan_sharp.sh
@@ -4,12 +4,12 @@
 # run this script in the ZY3 image folder contaning NAD images: e.g.,
 # ~/Data/Qinghai-Tibet/beiluhe/beiluhe_ZY3/ZY302_TMS_E92.7_N35.0_20171027_L1A0000345912
 
-export PATH=~/programs/StereoPipeline-2.6.1-2018-09-06-x86_64-OSX/bin:$PATH
+#export PATH=~/programs/StereoPipeline-2.6.1-2018-09-06-x86_64-OSX/bin:$PATH
 #export PATH=~/programs/StereoPipeline-2.6.1-2018-09-06-x86_64-Linux/bin:$PATH
-#export PATH=~/programs/StereoPipeline-2.6.1-2019-01-19-x86_64-Linux/bin:$PATH
+export PATH=~/programs/StereoPipeline-2.6.1-2019-01-19-x86_64-Linux/bin:$PATH
 
 #number of thread of to use, 8 or 16 on linux, 4 on mac (default is 4)
-num_thr=4
+num_thr=16
 
 #dem=~/Data/Qinghai-Tibet/beiluhe/DEM/srtm_30/beiluhe_strm30.tif
 dem=~/Data/Qinghai-Tibet/beiluhe/DEM/srtm_30/beiluhe_strm30_prj_utm.tif
@@ -19,6 +19,7 @@ out_res=2.1
 pan_res=2.1
 mss_res=5.8
 
+nodata=0
 # one solution: use NAD as the left image, FWD or BWD as the right image?
 gray=$(ls *-NAD.tiff)
 color=$(ls *-MUX.tiff)
@@ -32,7 +33,7 @@ IFS='_ ' read -r -a array <<< "$str"
 output=zy3_${array[4]}_${array[5]}_pansharp
 
 # ortho  first
-#mapproject -t rpc --tr ${pan_res} ${dem} ${gray} gray_mapped.tif \
+#mapproject -t rpc --nodata-value ${nodata} --tr ${pan_res} ${dem} ${gray} gray_mapped.tif \
 #        ${test_roi} --threads ${num_thr} --ot UInt16 --tif-compress None
 
 # error: Input images must be single channel or RGB!
@@ -49,7 +50,7 @@ for band in $(seq 1 4); do
     gdal_translate -b $band $color ${pre_name}.tif
     cp ${color_fname_no_ext}.rpb ${pre_name}.rpb
 
-    mapproject -t rpc --tr ${mss_res} ${dem} ${pre_name}.tif color_mapped_B${band}.tif \
+    mapproject -t rpc --nodata-value ${nodata} --tr ${mss_res} ${dem} ${pre_name}.tif color_mapped_B${band}.tif \
             ${test_roi} --threads ${num_thr} --ot UInt16 --tif-compress None
 
 #    exit
@@ -59,10 +60,19 @@ gdal_merge.py -separate -o color_mapped.tif color_mapped_B?.tif
 cp ${color_fname_no_ext}.rpb color_mapped.RPB
 
 
-pansharp --nodata-value 0 --threads ${num_thr} --tif-compress None \
- gray_mapped.tif color_mapped.tif ${output}.tif
-
+#pansharp --nodata-value 0 --threads ${num_thr} --tif-compress None \
+# gray_mapped.tif color_mapped.tif ${output}.tif
 
 # or using the pansharpening in OTB
 ~/codes/PycharmProjects/Landuse_DL/zy3Scripts/crop2theSameExtent.py color_mapped.tif gray_mapped.tif
-otbcli_Pansharpening -progress 1 -ram 2048 -inp gray_mapped.tif -inxs color_mapped_crop.tif -out ${output}_otb.tif uint16
+# method:rcs/lmvm/bayes , rcs has Segmentation fault (core dumped)
+otbcli_Pansharpening -progress 1 -method lmvm -inp gray_mapped.tif -inxs color_mapped_crop.tif -out ${output}_otb.tif uint16
+
+# convert the image for display purpose
+gdal_contrast_stretch -percentile-range 0.01 0.99 ${output}_otb.tif ${output}_otb_8bit.tif
+
+# the third band is red, second is green, and first is blue
+gdal_translate -b 3 -b 2 -b 1  ${output}_otb_8bit.tif ${output}_otb_8bit_rgb.tif
+
+# get a overview for visualization
+otbcli_Quicklook -progress 1 -sr 32 -in ${output}_otb_8bit_rgb.tif -out ${output}_otb_8bit_rgb_overview.tif uint8
