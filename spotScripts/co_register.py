b6b3f9b12c14e68bc024f34a8bc45b5a414599aa
yghlc <hlchuang@126.com>
2019-01-30 07:52:48 +0800

    add xml and parameter

--- spotScripts/co_register.py
+++ spotScripts/co_register.py
@@ -20,9 +20,9 @@ sys.path.insert(0, codes_dir2)
 # import basic_src.basic as basic
 import basic_src.io_function as io_function
 import basic_src.RSImageProcess as RSImageProcess
-import basic_src.geometryProcess as geometryProcess
 
 from basic_src.xml_rw import OffsetMetaDataClass
+import parameters
 
 def main(options, args):
     ref_image = args[0]
@@ -34,7 +34,8 @@ def main(options, args):
         output = io_function.get_name_by_adding_tail(new_image,'coreg')
 
     bkeepmidfile = True
-    coreg_xml = OffsetMetaDataClass()
+    xml_path=os.path.splitext(output)[0]+'.xml'
+    coreg_xml = OffsetMetaDataClass(xml_path)
 
     RSImageProcess.coregistration_siftGPU(ref_image,new_image,bkeepmidfile,coreg_xml)
 
@@ -48,9 +49,21 @@ if __name__ == "__main__":
                       action="store", dest="output",
                       help="the output file path")
 
+    parser.add_option("-p", "--para",
+                      action="store", dest="para_file",
+                      help="the parameters file")
+
     (options, args) = parser.parse_args()
     if len(sys.argv) < 2:
         parser.print_help()
         sys.exit(2)
+
+    ## set parameters files
+    if options.para_file is None:
+        print('error, no parameters file')
+        parser.print_help()
+        sys.exit(2)
+    else:
+        parameters.set_saved_parafile_path(options.para_file)
 
     main(options, args)
