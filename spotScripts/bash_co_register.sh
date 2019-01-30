6e14c3e7b472c9040314a7324f776c1c386eabb8
yghlc <hlchuang@126.com>
2019-01-30 10:39:57 +0800

    add

--- spotScripts/bash_co_register.sh
+++ spotScripts/bash_co_register.sh
@@ -11,7 +11,15 @@
 # Exit immediately if a command exits with a non-zero status. E: error trace
 set -eE -o functrace
 
-ref_img=''
+
+ref_img='../beiluhe_spot5_pan_20061109.tif'
+new_img='../beiluhe_spot5_pan_20090501.tif'
+para_file=para.ini
+
+~/codes/PycharmProjects/Landuse_DL/spotScripts/co_register.py ${ref_img} ${new_img} -p ${para_file}
+
+
+
 
 
 
