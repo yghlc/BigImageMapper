6f414e4a384acd78b49b1521a2cc001c34e46d85
Lingcao Huang <huanglingcao@gmail.com>
2019-02-26 22:11:26 -0700

    keep True pos only

---
+++ resultScript/keep_TruePos_only.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# eep the true positive only, i.e., remove polygons with IOU less than or equal to 0.5.
+
+#authors: Huang Lingcao
+#email:huanglingcao@gmail.com
+#add time: 26 February, 2019
+
+# Exit immediately if a command exits with a non-zero status. E: error trace
+set -eE -o functrace
+
+code_dir=~/codes/PycharmProjects/Landuse_DL
+# folder contains results
+res_dir=~/Data/Qinghai-Tibet/beiluhe/result/result_paper_mapping_RTS_dl_beiluhe
+
+### polygons
+# mapped
+shp_pre=${res_dir}/img_aug_test_results/BLH_basin_deeplabV3+_1_exp9_iter30000_post_imgAug16
+
+mainline_pre=${res_dir}/identified_ThawSlumps_MaiinLines_prj
+
+# remove False positive
+${code_dir}/resultScript/remove_polyongs.py ${shp_pre}.shp -o ${shp_pre}_TP.shp -f 'IoU' \
+    -t 0.5 -l ${mainline_pre}.shp --output_mainline=${mainline_pre}_TP.shp --bsmaller
+
+
+
+
+
+
+
+
