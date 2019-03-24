3e20a39c61b692ccf2f507e9425163ef7c9d66fd
Lingcao Huang <huanglingcao@gmail.com>
2019-03-24 16:42:55 -0600

    more comparison

--- landsat/batch_compare_to_gee.sh
+++ landsat/batch_compare_to_gee.sh
@@ -13,16 +13,21 @@ set -eE -o functrace
 gee_msi_dir=~/Data/Qinghai-Tibet/beiluhe/beiluhe_landsat/landsat_multispectral_indices
 msi_dir=./
 
-# compare ndvi
-./compare_to_gee.py landsat8_ndvi.tif ${gee_msi_dir}/beiluhe_LC08_NDVI_2013to2013.tif
-
-# compare ndwi
-./compare_to_gee.py landsat8_ndwi.tif ${gee_msi_dir}/beiluhe_LC08_NDWI_2013to2013.tif
+## compare ndvi
+#./compare_to_gee.py landsat8_ndvi.tif ${gee_msi_dir}/beiluhe_LC08_NDVI_2013to2013.tif
+#
+## compare ndwi
+#./compare_to_gee.py landsat8_ndwi.tif ${gee_msi_dir}/beiluhe_LC08_NDWI_2013to2013.tif
 
 # compare ndmi
-./compare_to_gee.py landsat8_ndmi.tif ${gee_msi_dir}/beiluhe_LC08_NDMI_2013to2013.tif
+#./compare_to_gee.py landsat8_ndmi.tif ${gee_msi_dir}/beiluhe_LC08_NDMI_2013to2013.tif
 
+# compare brightness
+#./compare_to_gee.py landsat8_brightness.tif ${gee_msi_dir}/beiluhe_LC08_brightness_2013to2013.tif
 
+#greenness
+./compare_to_gee.py landsat8_greenness.tif ${gee_msi_dir}/beiluhe_LC08_greenness_2013to2013.tif
 
-
+#wetness
+./compare_to_gee.py landsat8_wetness.tif ${gee_msi_dir}/beiluhe_LC08_wetness_2013to2013.tif
 
