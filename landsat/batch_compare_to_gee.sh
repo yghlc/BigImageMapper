28e9ca1d77884d6b12c737aa04e0b6365cdcd26e
Lingcao Huang <huanglingcao@gmail.com>
2019-03-24 15:59:17 -0600

    add

---
+++ landsat/batch_compare_to_gee.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# compare validate the multispectral indices (MSI) from Google Earth Engine
+# by comparing to the our calculation
+
+#authors: Huang Lingcao
+#email:huanglingcao@gmail.com
+#add time: 24 March, 2019
+
+# Exit immediately if a command exits with a non-zero status. E: error trace
+set -eE -o functrace
+
+gee_msi_dir=~/Data/Qinghai-Tibet/beiluhe/beiluhe_landsat/landsat_multispectral_indices
+msi_dir=./
+
+# compare ndvi
+./compare_to_gee.py landsat8_ndvi.tif ${gee_msi_dir}/beiluhe_LC08_NDVI_2013to2013.tif
+
+# compare ndwi
+./compare_to_gee.py landsat8_ndwi.tif ${gee_msi_dir}/beiluhe_LC08_NDWI_2013to2013.tif
+
+# compare ndmi
+./compare_to_gee.py landsat8_ndmi.tif ${gee_msi_dir}/beiluhe_LC08_NDMI_2013to2013.tif
+
+
+
+
+
