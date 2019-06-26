41b6084d94a70bf1c7ce3028001639312087a879
Lingcao Huang <huanglingcao@gmail.com>
2019-06-26 17:50:38 +0800

    add comments

--- landsat/cal_T-S_trend.sh
+++ landsat/cal_T-S_trend.sh
@@ -26,7 +26,9 @@ function index_trend() {
     mkdir -p ${index_name}_trend_patches
     rm ${index_name}_trend_patches/* || true
 
-    ${code_dir}/landsat/cal_TheilSen_trend.py ${input_tifs}  --name_index=${index_name} --annual_based
+    # the test show that annual single value is not good for calculating the trend.
+#    ${code_dir}/landsat/cal_TheilSen_trend.py ${input_tifs}  --name_index=${index_name} --annual_based
+    ${code_dir}/landsat/cal_TheilSen_trend.py ${input_tifs}  --name_index=${index_name}
 
     gdal_merge.py -o beiluhe_${index_name}_trend.tif  ${index_name}_trend_patches/*.tif
 
