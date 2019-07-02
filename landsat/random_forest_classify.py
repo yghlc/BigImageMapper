9df5744df1397cce8ac6f60dc15435547bebc5fd
Lingcao Huang <huanglingcao@gmail.com>
2019-07-02 17:32:19 +0800

    test

--- landsat/random_forest_classify.py
+++ landsat/random_forest_classify.py
@@ -280,25 +280,26 @@ class classify_pix_operation_rf(classify_pix_operation):
 
         # Number of trees in random forest
         n_estimators = [int(x) for x in np.linspace(start=200, stop=2000, num=10)]
-        # Number of features to consider at every split
-        max_features = ['auto', 'sqrt']
-        # Maximum number of levels in tree
-        max_depth = [int(x) for x in np.linspace(10, 110, num=11)]
-        max_depth.append(None)
-        # Minimum number of samples required to split a node
-        min_samples_split = [2, 5, 10]
-        # Minimum number of samples required at each leaf node
-        min_samples_leaf = [1, 2, 4]
-        # Method of selecting samples for training each tree
-        bootstrap = [True, False]
+        # # Number of features to consider at every split
+        # max_features = ['auto', 'sqrt']
+        # # Maximum number of levels in tree
+        # max_depth = [int(x) for x in np.linspace(10, 110, num=11)]
+        # max_depth.append(None)
+        # # Minimum number of samples required to split a node
+        # min_samples_split = [2, 5, 10]
+        # # Minimum number of samples required at each leaf node
+        # min_samples_leaf = [1, 2, 4]
+        # # Method of selecting samples for training each tree
+        # bootstrap = [True, False]
 
         # Create the serach grid
-        search_grid = {'n_estimators': n_estimators,
-                       'max_features': max_features,
-                       'max_depth': max_depth,
-                       'min_samples_split': min_samples_split,
-                       'min_samples_leaf': min_samples_leaf,
-                       'bootstrap': bootstrap}
+        search_grid = {'n_estimators': n_estimators}
+        # search_grid = {'n_estimators': n_estimators,
+        #                'max_features': max_features,
+        #                'max_depth': max_depth,
+        #                'min_samples_split': min_samples_split,
+        #                'min_samples_leaf': min_samples_leaf,
+        #                'bootstrap': bootstrap}
         basic.outputlogMessage(str(search_grid))
 
         clf = model_selection.GridSearchCV(RandomForestClassifier() , search_grid, cv=5,
