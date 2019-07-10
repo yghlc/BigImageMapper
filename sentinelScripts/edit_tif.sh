9e294e8822f89d38f31db13ebebb61d15ecc8563
Lingcao Huang <huanglingcao@gmail.com>
2019-07-10 15:40:43 +0800

    create a mosaic

--- sentinelScripts/edit_tif.sh
+++ sentinelScripts/edit_tif.sh
@@ -3,8 +3,15 @@
 # set nodata for images
 nodata=0
 
-for tif in $(ls *.tif); do
+#for tif in $(ls *.tif); do
+#
+#    gdal_edit.py -a_nodata ${nodata} ${tif}
+#done
 
-    gdal_edit.py -a_nodata ${nodata} ${tif}
-done
 
+tifs=$(ls sentinel-2_2018_mosaic_v2/*.tif)
+output=qtb_sentinel2_2018_mosaic_rgb_8bit_300m.tif
+#org_res=0.000089831528412
+res=0.0026949 # org_res*30
+
+gdal_merge.py -o ${output} -a_nodata 0 -ps ${res} ${res}  ${tifs}
