d397136c5f3eefc1705e15e5b05e283286b5ec9c
Lingcao Huang <huanglingcao@gmail.com>
2019-07-20 23:05:36 +0800

    useless

--- grss_data_fusion/deeplab_inference.py
+++ grss_data_fusion/deeplab_inference.py
@@ -267,9 +267,9 @@ def inference_one_patch(img_idx,idx,org_img_path,boundary,model):
     file_name = "I%d_%d"%(img_idx,idx) # short the file name to avoid  error of " Argument list too long", hlc 2018-Oct-29
 
     save_path = os.path.join(FLAGS.inf_output_dir,file_name+'.tif')
-    if os.path.isfile(save_path):
-        print('already exist, skip')
-        return True
+    # if os.path.isfile(save_path):
+    #     print('already exist, skip')
+    #     return True
     if build_RS_data.save_patch_oneband_8bit(img_patch,seg_map.astype(np.uint8),save_path) is False:
         return False
 
@@ -362,10 +362,10 @@ def inf_remoteSensing_image(model,image_path=None):
                 file_name = "I%d_%d" % (img_idx, idx)
 
                 save_path = os.path.join(FLAGS.inf_output_dir, file_name + '.tif')
-                if os.path.isfile(save_path):
-                    print('already exist, skip')
-                    idx += 1
-                    continue
+                # if os.path.isfile(save_path):
+                #     print('already exist, skip')
+                #     idx += 1
+                #     continue
                 if build_RS_data.save_patch_oneband_8bit(img_patch,seg_map.astype(np.uint8),save_path) is False:
                     return False
 
