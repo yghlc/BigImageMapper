6ac78bb9d94d0685f392566eb05c85b75ffbb101
Lingcao Huang <huanglingcao@gmail.com>
2019-08-10 17:07:57 +0800

    bug fix

--- sentinelScripts/copy_to_service.sh
+++ sentinelScripts/copy_to_service.sh
@@ -11,28 +11,28 @@
 function copy_files() {
     file_head=$1
 
-    scp $1 $chpc_host:/project/LinLiu/hlc/Data/Qinghai-Tibet/entire_QTP_images/sentinel-2/8bit_dir/sentinel-2_2018_mosaic_v3/.
+    scp ${file_head}* $chpc_host:/project/LinLiu/hlc/Data/Qinghai-Tibet/entire_QTP_images/sentinel-2/8bit_dir/sentinel-2_2018_mosaic_v3/.
 }
 
 
-copy_files qtb_sentinel2_2018_mosaic-0000000000*
+copy_files qtb_sentinel2_2018_mosaic-0000000000
 # stop, then change the group info on ITSC services
 
-#copy_files qtb_sentinel2_2018_mosaic-0000026880*
+#copy_files qtb_sentinel2_2018_mosaic-0000026880
 
-#copy_files qtb_sentinel2_2018_mosaic-0000053760*
+#copy_files qtb_sentinel2_2018_mosaic-0000053760
 
-#copy_files qtb_sentinel2_2018_mosaic-0000080640*
+#copy_files qtb_sentinel2_2018_mosaic-0000080640
 
-#copy_files qtb_sentinel2_2018_mosaic-0000107520*
+#copy_files qtb_sentinel2_2018_mosaic-0000107520
 
-#copy_files qtb_sentinel2_2018_mosaic-0000134400*
+#copy_files qtb_sentinel2_2018_mosaic-0000134400
 
 
-# copy txt and sh files (no need)
+# copy txt and sh files (manually)
 
-copy_files *.txt
-copy_files *.sh
+#copy_files *.txt
+#copy_files *.sh
 
 
 
