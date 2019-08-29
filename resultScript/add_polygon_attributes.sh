b327a3291af34aa737b0e44362a0fbea0effbd51
Lingcao Huang <huanglingcao@gmail.com>
2019-08-29 21:32:34 +0800

    added

--- resultScript/add_polygon_attributes.sh
+++ resultScript/add_polygon_attributes.sh
@@ -11,25 +11,31 @@ set -eE -o functrace
 
 code_dir=~/codes/PycharmProjects/Landuse_DL
 # folder contains results
-res_dir=~/Data/Qinghai-Tibet/beiluhe/result/result_paper_mapping_RTS_dl_beiluhe
+#res_dir=~/Data/Qinghai-Tibet/beiluhe/result/result_paper_mapping_RTS_dl_beiluhe
+res_dir=~/Data/Qinghai-Tibet/beiluhe/result/result_paper_mapping_RTS_dl_beiluhe_revised2019
 
 ### polygons
-# the 202 ground truth polygons
-ground_truth=${res_dir}/identified_ThawSlumps_prj_post.shp
-#polygon_shp=${res_dir}/identified_ThawSlumps_prj_post.shp
-# the 165 true positives
-polygon_shp=${res_dir}/img_aug_test_results/BLH_basin_deeplabV3+_1_exp9_iter30000_post_imgAug16_TP.shp
+# the 264 ground truth polygons
+#ground_truth=${res_dir}/identified_ThawSlumps_prj_post.shp
+ground_truth=${res_dir}/identified_thawslumps_utm_post.shp
+#polygon_shp=${ground_truth}
 
-# polyon without post-processing (removing polygons based on their areas)
-shp_imgAug16_NOpost=${res_dir}/img_aug_test_results/BLH_basin_deeplabV3+_1_exp9_iter30000_imgAug16.shp
-shp_imgAug17_NOpost=${res_dir}/img_aug_test_results/BLH_basin_deeplabV3+_1_exp9_iter30000_imgAug17.shp
+## the 165 true positives
+#polygon_shp=${res_dir}/img_aug_test_results/BLH_basin_deeplabV3+_1_exp9_iter30000_post_imgAug16_TP.shp
+
+# the 220 true positives
+polygon_shp=${res_dir}/img_aug_test_results/BLH_basin_deeplabV3+_1_b_exp10_iter30000_post_imgAug22_TP.shp
+
+## polyon without post-processing (removing polygons based on their areas)
+#shp_imgAug16_NOpost=${res_dir}/img_aug_test_results/BLH_basin_deeplabV3+_1_exp9_iter30000_imgAug16.shp
+#shp_imgAug17_NOpost=${res_dir}/img_aug_test_results/BLH_basin_deeplabV3+_1_exp9_iter30000_imgAug17.shp
 
 ################################################################################
 ### polygon geometric information and IoU
 
 #${code_dir}/resultScript/add_info2Pylygons.py ${shp_imgAug16_NOpost} -v ${ground_truth} -n "IoU"
 
-${code_dir}/resultScript/add_info2Pylygons.py ${shp_imgAug17_NOpost} -v ${ground_truth} -n "IoU"
+#${code_dir}/resultScript/add_info2Pylygons.py ${shp_imgAug17_NOpost} -v ${ground_truth} -n "IoU"
 
 ################################################################################
 ### raster
@@ -41,15 +47,15 @@ slope=~/Data/Qinghai-Tibet/beiluhe/DEM/srtm_30/beiluhe_srtm30_utm_basinExt_slope
 aspect=~/Data/Qinghai-Tibet/beiluhe/DEM/srtm_30/beiluhe_srtm30_utm_basinExt_apect.tif
 
 # inside polygons
-#${code_dir}/resultScript/add_info2Pylygons.py ${polygon_shp} -r ${pisr} -n "pisr"
+${code_dir}/resultScript/add_info2Pylygons.py ${polygon_shp} -r ${pisr} -n "pisr"
+
+${code_dir}/resultScript/add_info2Pylygons.py ${polygon_shp} -r ${tpi} -n "tpi"
 #
-#${code_dir}/resultScript/add_info2Pylygons.py ${polygon_shp} -r ${tpi} -n "tpi"
-#
-#${code_dir}/resultScript/add_info2Pylygons.py ${polygon_shp} -r ${slope} -n "slo"
-#
-#${code_dir}/resultScript/add_info2Pylygons.py ${polygon_shp} -r ${dem} -n "dem"
-#
-#${code_dir}/resultScript/add_info2Pylygons.py ${polygon_shp} -r ${aspect} -n "asp"
+${code_dir}/resultScript/add_info2Pylygons.py ${polygon_shp} -r ${slope} -n "slo"
+
+${code_dir}/resultScript/add_info2Pylygons.py ${polygon_shp} -r ${dem} -n "dem"
+
+${code_dir}/resultScript/add_info2Pylygons.py ${polygon_shp} -r ${aspect} -n "asp"
 
 
 
