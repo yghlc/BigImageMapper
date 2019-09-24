342ca1511c0ac12007c566f34d868e3f063b07e4
yghlc <hlcgithub123>
2019-09-24 18:58:24 +0800

    backup

--- sentinelScripts/parallel_predict_rts.py
+++ sentinelScripts/parallel_predict_rts.py
@@ -9,8 +9,19 @@ add time: 24 September, 2019
 """
 
 import os, sys
+import os, sys
+HOME = os.path.expanduser('~')
+codes_dir2 = HOME +'/codes/PycharmProjects/DeeplabforRS'
+sys.path.insert(0, codes_dir2)
+
+import basic_src.basic as basic
 
 # get GPU information on the machine
+
+gpu_str = basic  os.system('lspci | grep -i nvidia')
+for line_str in gpu_str:
+    if 'VGA compatible controller: NVIDIA' in line_str:
+
 
 # get
 
