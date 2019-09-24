c7754912cf3fb50a6f7b550e7bdee6f909e1e2cb
yghlc <hlchuang@126.com>
2019-09-24 11:49:17 +0800

    get sub-images based on coordinates

--- sentinelScripts/create_inf_image_list.py
+++ sentinelScripts/create_inf_image_list.py
@@ -34,6 +34,12 @@ import basic_src.io_function as io_function
 # qtb_sentinel2_2018_mosaic-0000026880-0000107520_8bit.tif
 # qtb_sentinel2_2018_mosaic-0000026880-0000080640_8bit.tif
 
+# selected subImage for test, these a upper-left coordinate of images (created by Gogole Earth Engine)
+selected_subImage = ['0000053760-0000268800','0000053760-0000241920','0000053760-0000215040','0000053760-0000188160',
+                     '0000053760-0000161280','0000053760-0000134400','0000053760-0000107520','0000053760-0000080640',
+                     '0000026880-0000268800','0000026880-0000241920','0000026880-0000215040','0000026880-0000188160',
+                     '0000026880-0000161280','0000026880-0000134400','0000026880-0000107520','0000026880-0000080640']
+
 # get image list
 image_dir = HOME + '/Data/Qinghai-Tibet/entire_QTP_images/sentinel-2/8bit_dir/sentinel-2_2018_mosaic_v4'
 
@@ -42,8 +48,22 @@ image_files = io_function.get_file_list_by_ext('.tif',image_dir,bsub_folder=Fals
 image_count = len(image_files)
 
 
-# output all file name
-with open('inf_image_list.txt') as list_obj:
+# output file list
+with open('inf_image_list.txt','w') as list_obj:
     for image_path in image_files:
-        list_obj.writelines(os.path.basename(image_path)[1]+'\n')
+
+        name  = os.path.basename(image_path)
+        # print(name)
+
+        # get upper-left coordinate
+        str_list = name.split('_')
+        tmp_str = str_list[-2]
+        coordinate = tmp_str.split('-')[-2] + '-' + tmp_str.split('-')[-1]
+        # print(coordinate)
+
+        if coordinate in selected_subImage:
+            list_obj.writelines(name+'\n')
+        else:
+            continue
+
 
