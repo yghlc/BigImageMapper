fc8ddf9c247eda01fee815c54efcbcab392047a0
Lingcao Huang <huanglingcao@gmail.com>
2019-10-13 19:31:35 +0800

    test

--- sentinelScripts/post_pro_chpc.sh
+++ sentinelScripts/post_pro_chpc.sh
@@ -15,10 +15,10 @@ eo_dir=~/codes/PycharmProjects/Landuse_DL
 
 script=~/codes/PycharmProjects/Landuse_DL/sentinelScripts/copyTolocal_postPro.py
 
-#${script} para_qtp.ini
+${script} para_qtp.ini
 # parallel run this, delay 200 second for each job
-parallel --progress --delay 200 ${script} ${script} ${script} ${script} ${script} ${script} ${script} ${script} \
-::: para_qtp.ini para_qtp.ini para_qtp.ini para_qtp.ini para_qtp.ini para_qtp.ini para_qtp.ini para_qtp.ini
+#parallel --progress --delay 200 ${script} ${script} ${script} ${script} ${script} ${script} ${script} ${script} \
+#::: para_qtp.ini para_qtp.ini para_qtp.ini para_qtp.ini para_qtp.ini para_qtp.ini para_qtp.ini para_qtp.ini
 
 
 ## post processing and copy results, including output "time_cost.txt"
