6059e4007256c377f51eeda0beeb704840c72e22
Lingcao Huang <huanglingcao@gmail.com>
2019-10-16 08:53:30 +0800

    added

--- sentinelScripts/rm_unCompleted_task.py
+++ sentinelScripts/rm_unCompleted_task.py
@@ -11,10 +11,10 @@ add time: 16 October, 2019
 import os
 dir = 'multi_inf_results'
 for i in range(0,78):
-    if os.path.join(dir,str(i)+'.txt'):
-        if os.path.join(dir,str(i)+'.txt_done'):
+    if os.path.isfile(os.path.join(dir,str(i)+'.txt')):
+        if os.path.isfile(os.path.join(dir,str(i)+'.txt_done')):
             print('task %d is completed'%i)
         else:
             print('task %d is not completed, remove the folder' % i)
-            os.system('rm -rf '+ 'I%d'%i)
+            # os.system('rm -rf '+ 'I%d'%i)
 
