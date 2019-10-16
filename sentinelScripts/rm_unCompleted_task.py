4e2fc7c7005bee8ab69902e9e2128fd9c604fe3f
Lingcao Huang <huanglingcao@gmail.com>
2019-10-16 08:59:50 +0800

    bug fix

--- sentinelScripts/rm_unCompleted_task.py
+++ sentinelScripts/rm_unCompleted_task.py
@@ -16,5 +16,6 @@ for i in range(0,78):
             print('task %d is completed'%i)
         else:
             print('task %d is not completed, remove the folder' % i)
-            os.system('rm -rf '+ 'I%d'%i)
+            folder = os.path.join(dir,str(i)+'I%d'%i)
+            os.system('rm -rf '+ folder)
 
