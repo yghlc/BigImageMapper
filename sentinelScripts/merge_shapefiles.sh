751c1c9df32c4efc8dccecc786a0c48da2f9416a
Lingcao Huang <huanglingcao@gmail.com>
2020-07-27 10:57:26 +0800

    use python to merge shapefiles

--- sentinelScripts/merge_shapefiles.sh
+++ sentinelScripts/merge_shapefiles.sh
@@ -9,6 +9,8 @@
 # Exit immediately if a command exits with a non-zero status. E: error trace
 set -eE -o functrace
 
+
+
 # input a parameter: the path of para_file (e.g., para.ini)
 para_file=$1
 if [ ! -f $para_file ]; then
@@ -16,6 +18,10 @@ if [ ! -f $para_file ]; then
    exit 1
 fi
 para_py=~/codes/PycharmProjects/DeeplabforRS/parameters.py
+
+# use python3 for running this script
+export PATH=~/programs/anaconda3/bin:$PATH
+merge_shp_py=~/codes/PycharmProjects/ChangeDet_DL/dataTools/merge_shapefiles.py
 
 test=$2
 
@@ -29,45 +35,56 @@ cd result_backup
 
 tile_dir=${testid}_${test}_tiles
 
-# crop a data set
-function merge_shp() {
-    out_shp=$1
-    out_name=$2
-    i=$3
-
-    if [ -f "$out_shp" ]
-    then
-       echo "updating ${out_shp}"
-       ogr2ogr -f "ESRI Shapefile" -update -append $out_shp $i -nln ${out_name}
-    else
-        echo "creating ${out_shp}"
-        ogr2ogr -f "ESRI Shapefile" $out_shp $i
-    fi
-}
+################################################
+## merge shape file using python
+${merge_shp_py} ${tile_dir}
 
 
-for first_shp in $(ls ${tile_dir}/I0_*.shp); do
+################################################
+# merge shape file using bash
+#function merge_shp() {
+#    out_shp=$1
+#    out_name=$2
+#    i=$3
+#
+#    # the first shape file cannot be empty, otherwise, all the field in the following would be skipped
+#    # need to add script to check
+#
+#    if [ -f "$out_shp" ]
+#    then
+#       echo "updating ${out_shp}"
+#       ogr2ogr -f "ESRI Shapefile" -update -append $out_shp $i -nln ${out_name}
+#    else
+#        echo "creating ${out_shp}"
+#        ogr2ogr -f "ESRI Shapefile" $out_shp $i
+#    fi
+#}
+#
+#
+#for first_shp in $(ls ${tile_dir}/I0_*.shp); do
+#
+##    out_name=${testid}_prj_post_${test}
+#    echo "First sub shape file:" $first_shp
+#    basename=$(basename $first_shp)
+#    # remove "I0_"
+#    out_shp=$(python -c "import sys; print('_'.join(sys.argv[1].split('_')[1:]))" $basename)
+#    out_name="${out_shp%.*}"
+#    if [ -f $out_shp ]; then
+#        echo "remove previous results"
+#        rm ${out_name}.*
+#    fi
+#    for i in $(ls ${tile_dir}/I*_${out_name}.shp)
+#    do
+#        echo "merging $i"
+#        merge_shp ${out_shp} ${out_name} ${i}
+#    done
+#    # convert to KML
+#    echo $"convert to KML format"
+#    ogr2ogr -f KML ${out_name}.kml ${out_shp}
+#
+#done
 
-#    out_name=${testid}_prj_post_${test}
-    echo "First sub shape file:" $first_shp
-    basename=$(basename $first_shp)
-    # remove "I0_"
-    out_shp=$(python -c "import sys; print('_'.join(sys.argv[1].split('_')[1:]))" $basename)
-    out_name="${out_shp%.*}"
-    if [ -f $out_shp ]; then
-        echo "remove previous results"
-        rm ${out_name}.*
-    fi
-    for i in $(ls ${tile_dir}/I*_${out_name}.shp)
-    do
-        echo "merging $i"
-        merge_shp ${out_shp} ${out_name} ${i}
-    done
-    # convert to KML
-    echo $"convert to KML format"
-    ogr2ogr -f KML ${out_name}.kml ${out_shp}
-
-done
+################################################
 
 
 #out_name=${testid}_prj_${test}
