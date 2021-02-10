a516c8ee7fe45a47352eab96a7d46427f75b15a6
Lingcao Huang <huanglingcao@gmail.com>
2021-02-10 11:55:48 -0700

    parallel split images

--- workflow/split_sub_images.py
+++ workflow/split_sub_images.py
@@ -22,6 +22,9 @@ import basic_src.io_function as io_function
 
 import workflow.split_train_val as split_train_val
 
+import multiprocessing
+from multiprocessing import Pool
+
 def split_to_patches(image_path, out_dir, patch_width, patch_height, overlay_x, overlay_y, out_format, file_pre_name=None):
     patch_width = int(patch_width)
     patch_height = int(patch_height)
@@ -33,6 +36,15 @@ def split_to_patches(image_path, out_dir, patch_width, patch_height, overlay_x, 
 
     split_image.split_image(image_path, out_dir, patch_width, patch_height, overlay_x, overlay_y, out_format, pre_name=file_pre_name)
 
+def split_a_pair_sub_image_label(line, patch_w, patch_h, overlay_x, overlay_y, split_image_format):
+
+    sub_image, sub_label = line.split(':')
+    # split sub image
+    split_to_patches(sub_image, 'split_images', patch_w, patch_h, overlay_x, overlay_y, split_image_format)
+
+    # split sub label (change the file name to be the same as sub_image name)
+    pre_name = os.path.splitext(os.path.basename(sub_image))[0]
+    split_to_patches(sub_label, 'split_labels', patch_w, patch_h, overlay_x, overlay_y, split_image_format, file_pre_name=pre_name)
 
 def main(options, args):
 
@@ -54,11 +66,13 @@ def main(options, args):
     ### split the training image to many small patch (480*480)
     patch_w=parameters.get_string_parameters(para_file,'train_patch_width')
     patch_h=parameters.get_string_parameters(para_file,'train_patch_height')
-    overlay=parameters.get_string_parameters(para_file,'train_pixel_overlay_x')
+    overlay_x=parameters.get_string_parameters(para_file,'train_pixel_overlay_x')
+    overlay_y=parameters.get_string_parameters(para_file,'train_pixel_overlay_y')
     split_image_format=parameters.get_string_parameters(para_file,'split_image_format')
 
     trainImg_dir=parameters.get_string_parameters(para_file,'input_train_dir')
     labelImg_dir=parameters.get_string_parameters(para_file,'input_label_dir')
+    proc_num = parameters.get_digit_parameters(para_file,'process_num','int')
 
     if os.path.isdir(trainImg_dir) is False:
         raise IOError('%s not in the current folder, please get subImages first'%trainImg_dir)
@@ -70,15 +84,19 @@ def main(options, args):
 
     with open(sub_img_label_txt) as txt_obj:
         line_list = [name.strip() for name in txt_obj.readlines()]
-        for line in line_list:
-            sub_image, sub_label = line.split(':')
+        # for line in line_list:
+        #     sub_image, sub_label = line.split(':')
+        #
+        #     # split sub image
+        #     split_to_patches(sub_image, 'split_images', patch_w, patch_h, overlay, overlay, split_image_format)
+        #
+        #     # split sub label (change the file name to be the same as sub_image name)
+        #     pre_name = os.path.splitext(os.path.basename(sub_image))[0]
+        #     split_to_patches(sub_label, 'split_labels', patch_w, patch_h, overlay, overlay, split_image_format, file_pre_name=pre_name)
 
-            # split sub image
-            split_to_patches(sub_image, 'split_images', patch_w, patch_h, overlay, overlay, split_image_format)
-
-            # split sub label (change the file name to be the same as sub_image name)
-            pre_name = os.path.splitext(os.path.basename(sub_image))[0]
-            split_to_patches(sub_label, 'split_labels', patch_w, patch_h, overlay, overlay, split_image_format, file_pre_name=pre_name)
+        parameters_list = [(line, patch_w, patch_h, overlay_x, overlay_y, split_image_format) for line in line_list]
+        theadPool = Pool(proc_num)  # multi processes
+        results = theadPool.starmap(split_a_pair_sub_image_label, parameters_list)  # need python3
 
         # output trainval.txt and val.txt file
         files_list = io_function.get_file_list_by_ext(split_image_format, 'split_images',bsub_folder=False)
