5e3b67e0eadd4452d5a6970fca2ffcbfd41e61b5
Lingcao Huang <huanglingcao@gmail.com>
2021-02-14 20:00:47 -0700

    train eval use different process

--- workflow/deeplab_train.py
+++ workflow/deeplab_train.py
@@ -15,6 +15,7 @@ import re
 import time
 
 import numpy as np
+from multiprocessing import Process
 
 code_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
 sys.path.insert(0, code_dir)
@@ -116,8 +117,11 @@ def train_deeplab(train_script,dataset,train_split,num_of_classes,base_learning_
 
 def evaluation_deeplab(evl_script,dataset, evl_split,num_of_classes, model_variant,
                        atrous_rates1,atrous_rates2,atrous_rates3,output_stride,train_logdir, evl_logdir,dataset_dir,
-                       crop_size_str,max_eva_number,depth_multiplier,decoder_output_stride,aspp_convs_filters):
+                       crop_size_str,max_eva_number,depth_multiplier,decoder_output_stride,aspp_convs_filters,gpuid=None,
+                       eval_interval_secs=300):
 
+    if gpuid is not None:
+        os.environ['CUDA_VISIBLE_DEVICES'] = str(gpuid)
     # for information, run "python deeplab/eval.py  --helpfull"
 
 # --eval_interval_secs: How often (in seconds) to run evaluation.
@@ -135,6 +139,7 @@ def evaluation_deeplab(evl_script,dataset, evl_split,num_of_classes, model_varia
                      + ' --checkpoint_dir=' + train_logdir \
                      + ' --eval_logdir=' + evl_logdir \
                      + ' --dataset_dir=' + dataset_dir \
+                     + ' --eval_interval_secs=' + str(eval_interval_secs) \
                      + ' --max_number_of_evaluations=' + str(max_eva_number)
 
     if output_stride is not None:
@@ -447,6 +452,179 @@ def train_evaluation_deeplab(WORK_DIR,deeplab_dir,expr_name, para_file, network_
     loss_curve_bakname = os.path.join(backup_dir, test_id+ '_'+os.path.basename(loss_curve_path))
     io_function.copy_file_to_dst(loss_curve_path, loss_curve_bakname, overwrite=True)
 
+def train_evaluation_deeplab_separate(WORK_DIR,deeplab_dir,expr_name, para_file, network_setting_ini,gpu_num):
+    '''
+    in "train_evaluation_deeplab", run training, stop, then evaluation, then traininng, make learning rate strange, and the results worse.
+    so in this function, we start two process, one for training, another for evaluation (run on CPU)
+    '''
+    # prepare training folder
+    EXP_FOLDER = expr_name
+    INIT_FOLDER =  os.path.join(WORK_DIR,EXP_FOLDER,'init_models')
+    TRAIN_LOGDIR = os.path.join(WORK_DIR,EXP_FOLDER,'train')
+    EVAL_LOGDIR = os.path.join(WORK_DIR,EXP_FOLDER,'eval')
+    VIS_LOGDIR = os.path.join(WORK_DIR,EXP_FOLDER,'vis')
+    EXPORT_DIR = os.path.join(WORK_DIR,EXP_FOLDER,'export')
+
+    io_function.mkdir(INIT_FOLDER)
+    io_function.mkdir(TRAIN_LOGDIR)
+    io_function.mkdir(EVAL_LOGDIR)
+    io_function.mkdir(VIS_LOGDIR)
+    io_function.mkdir(EXPORT_DIR)
+
+    # prepare the tensorflow check point (pretrained model) for training
+    pre_trained_dir = parameters.get_directory_None_if_absence(network_setting_ini, 'pre_trained_model_folder')
+    pre_trained_tar = parameters.get_string_parameters(network_setting_ini, 'TF_INIT_CKPT')
+    pre_trained_path = os.path.join(pre_trained_dir, pre_trained_tar)
+    if os.path.isfile(pre_trained_path) is False:
+        print('pre-trained model: %s not exist, try to download'%pre_trained_path)
+        # try to download the file
+        pre_trained_url = parameters.get_string_parameters_None_if_absence(network_setting_ini, 'pre_trained_model_url')
+        res  = os.system('wget %s '%pre_trained_url)
+        if res != 0:
+            sys.exit(1)
+        io_function.movefiletodir(pre_trained_tar,pre_trained_dir)
+
+    # unpack pre-trained model to INIT_FOLDER
+    os.chdir(INIT_FOLDER)
+    res = os.system('tar -xf %s'% pre_trained_path)
+    if res != 0:
+        raise IOError('failed to unpack %s'%pre_trained_path)
+    os.chdir(WORK_DIR)
+
+    dataset_dir = os.path.join(WORK_DIR, 'tfrecord')
+    batch_size = parameters.get_digit_parameters(network_setting_ini,'batch_size','int')
+    # maximum iteration number
+    iteration_num = parameters.get_digit_parameters(network_setting_ini,'iteration_num','int')
+    base_learning_rate = parameters.get_digit_parameters(network_setting_ini,'base_learning_rate','float')
+
+    train_output_stride = parameters.get_digit_parameters_None_if_absence(network_setting_ini,'train_output_stride','int')
+    train_atrous_rates1 = parameters.get_digit_parameters_None_if_absence(network_setting_ini,'train_atrous_rates1','int')
+    train_atrous_rates2 = parameters.get_digit_parameters_None_if_absence(network_setting_ini,'train_atrous_rates2','int')
+    train_atrous_rates3 = parameters.get_digit_parameters_None_if_absence(network_setting_ini,'train_atrous_rates3','int')
+
+    inf_output_stride = parameters.get_digit_parameters_None_if_absence(network_setting_ini,'inf_output_stride','int')
+    inf_atrous_rates1 = parameters.get_digit_parameters_None_if_absence(network_setting_ini,'inf_atrous_rates1','int')
+    inf_atrous_rates2 = parameters.get_digit_parameters_None_if_absence(network_setting_ini,'inf_atrous_rates2','int')
+    inf_atrous_rates3 = parameters.get_digit_parameters_None_if_absence(network_setting_ini,'inf_atrous_rates3','int')
+
+    # depth_multiplier default is 1.0.
+    depth_multiplier = parameters.get_digit_parameters_None_if_absence(network_setting_ini, 'depth_multiplier', 'float')
+
+    decoder_output_stride = parameters.get_digit_parameters_None_if_absence(network_setting_ini, 'decoder_output_stride', 'int')
+    aspp_convs_filters = parameters.get_digit_parameters_None_if_absence(network_setting_ini, 'aspp_convs_filters','int')
+
+    train_script = os.path.join(deeplab_dir, 'train.py')
+    train_split = os.path.splitext(parameters.get_string_parameters(para_file,'training_sample_list_txt'))[0]
+    model_variant = parameters.get_string_parameters(network_setting_ini, 'model_variant')
+    checkpoint = parameters.get_string_parameters(network_setting_ini, 'tf_initial_checkpoint')
+    init_checkpoint_files = io_function.get_file_list_by_pattern(INIT_FOLDER, checkpoint + '*')
+    if len(init_checkpoint_files) < 1:
+        raise IOError('No initial checkpoint in %s with pattern: %s'%(INIT_FOLDER,checkpoint))
+    init_checkpoint = os.path.join(INIT_FOLDER,checkpoint)
+    dataset = parameters.get_string_parameters(para_file,'dataset_name')
+    num_classes_noBG = parameters.get_digit_parameters_None_if_absence(para_file, 'NUM_CLASSES_noBG', 'int')
+    assert num_classes_noBG != None
+    if pre_trained_tar in pre_trained_tar_21_classes:
+        print('warning, pretrained model %s is trained with 21 classes, set num_of_classes to 21'%pre_trained_tar)
+        num_classes_noBG = 20
+    if pre_trained_tar in pre_trained_tar_19_classes:
+        print('warning, pretrained model %s is trained with 19 classes, set num_of_classes to 19'%pre_trained_tar)
+        num_classes_noBG = 18
+    num_of_classes = num_classes_noBG + 1
+
+    image_crop_size = parameters.get_string_list_parameters(para_file, 'image_crop_size')
+    if len(image_crop_size) != 2 and image_crop_size[0].isdigit() and image_crop_size[1].isdigit():
+        raise ValueError('image_crop_size should be height,width')
+    crop_size_str = ','.join(image_crop_size)
+
+
+    b_early_stopping = parameters.get_bool_parameters(para_file,'b_early_stopping')
+
+    # validation interval (epoch), do
+    # validation_interval = parameters.get_digit_parameters_None_if_absence(para_file,'validation_interval','int')
+
+
+    train_count, val_count = get_train_val_sample_count(WORK_DIR, para_file)
+    iter_per_epoch = math.ceil(train_count/batch_size)
+    total_epoches = math.ceil(iteration_num/iter_per_epoch)
+    already_trained_iteration  = get_trained_iteration(TRAIN_LOGDIR)
+    if already_trained_iteration >= iteration_num:
+        basic.outputlogMessage('Training already run %d iterations, skip'%already_trained_iteration)
+        return True
+
+    save_interval_secs = 1200   # default is 1200 second for saving model
+    save_summaries_secs = 600   # default is 600 second for saving summaries
+    eval_interval_secs = save_interval_secs    # default is 300 second for running evaluation, if no new saved model, no need to run evaluation?
+
+    train_process = Process(target=train_deeplab,
+                          args=(train_script,dataset, train_split,num_of_classes, base_learning_rate, model_variant, init_checkpoint, TRAIN_LOGDIR,
+                      dataset_dir, gpu_num,
+                      train_atrous_rates1, train_atrous_rates2, train_atrous_rates3, train_output_stride,crop_size_str, batch_size,iteration_num,
+                      depth_multiplier,decoder_output_stride,aspp_convs_filters))
+    train_process.start()
+    time.sleep(20)  # wait
+    if train_process.exitcode is not None and train_process.exitcode != 0:
+        sys.exit(1)
+
+    gpuid = ""  # set gpuid to empty string, making evaluation run on CPU
+    evl_script = os.path.join(deeplab_dir, 'eval.py')
+    evl_split = os.path.splitext(parameters.get_string_parameters(para_file,'validation_sample_list_txt'))[0]
+    max_eva_number = -1  # run as many evaluation as possible, --eval_interval_secs (default is 300 seconds)
+    eval_process = Process(target=evaluation_deeplab,
+                           args=(evl_script,dataset, evl_split, num_of_classes,model_variant,
+                           inf_atrous_rates1,inf_atrous_rates2,inf_atrous_rates3,inf_output_stride,TRAIN_LOGDIR, EVAL_LOGDIR,
+                           dataset_dir,crop_size_str, max_eva_number,depth_multiplier,decoder_output_stride,aspp_convs_filters,
+                                 gpuid,eval_interval_secs))
+    eval_process.start()
+    time.sleep(10)  # wait
+    if eval_process.exitcode is not None and eval_process.exitcode != 0:
+        sys.exit(1)
+
+    while True:
+        # check if need early stopping
+        if b_early_stopping:
+            miou_dict = get_miou_list_class_all(EVAL_LOGDIR, num_of_classes)
+            if len(miou_dict['overall']) >= 5:
+                # if the last five miou did not improve, then stop training
+                if np.all(np.diff(miou_dict['overall'][-5:]) < 0.0001):
+                    basic.outputlogMessage(
+                        'early stopping: stop training because overall miou did not improved in the last five evaluation')
+                    train_process.kill()
+                    eval_process.kill()
+
+        # if finished training
+        if train_process.is_alive() is False:
+            eval_process.kill()
+            break
+        time.sleep(30)  # wait
+
+    # save loss value to disk
+    get_loss_learning_rate_list(TRAIN_LOGDIR)
+    # get miou again
+    miou_dict = get_miou_list_class_all(EVAL_LOGDIR, num_of_classes)
+
+
+    # plot mIOU, loss, and learnint rate curves
+    miou_curve_path = plot_miou_loss_curve.plot_miou_loss_main('miou.txt',train_count=train_count, val_count=val_count,batch_size=batch_size)
+    loss_curve_path = plot_miou_loss_curve.plot_miou_loss_main('loss_learning_rate.txt',train_count=train_count, val_count=val_count,batch_size=batch_size)
+
+    # backup miou and training_loss & learning rate
+    test_id = os.path.basename(WORK_DIR) + '_' + expr_name
+    backup_dir = os.path.join(WORK_DIR, 'result_backup')
+    if os.path.isdir(backup_dir) is False:
+        io_function.mkdir(backup_dir)
+    iou_path = os.path.join(EVAL_LOGDIR, 'miou.txt')
+    new_iou_name = os.path.join(backup_dir, test_id+ '_'+os.path.basename(iou_path))
+    io_function.copy_file_to_dst(iou_path, new_iou_name, overwrite=True)
+    miou_curve_bakname = os.path.join(backup_dir, test_id+ '_'+os.path.basename(miou_curve_path))
+    io_function.copy_file_to_dst(miou_curve_path, miou_curve_bakname, overwrite=True)
+
+
+    loss_path = os.path.join(TRAIN_LOGDIR, 'loss_learning_rate.txt')
+    loss_new_name = os.path.join(backup_dir,test_id+ '_'+os.path.basename(loss_path))
+    io_function.copy_file_to_dst(loss_path, loss_new_name, overwrite=True)
+    loss_curve_bakname = os.path.join(backup_dir, test_id+ '_'+os.path.basename(loss_curve_path))
+    io_function.copy_file_to_dst(loss_curve_path, loss_curve_bakname, overwrite=True)
 
 # def init_for_test_function():
 #     code_dir = os.path.expanduser('~/codes/PycharmProjects/Landuse_DL')
