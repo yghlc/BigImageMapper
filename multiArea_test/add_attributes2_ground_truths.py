0eaa62894595e0dd14fac0143cc9a5ebf3eab560
Lingcao Huang <huanglingcao@gmail.com>
2021-02-16 07:59:33 -0700

    get info for ground truth

---
+++ multiArea_test/add_attributes2_ground_truths.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# Filename: add_attributes2_ground_truths.py 
+"""
+introduction: add attributes to multiple ground truth polygons
+
+authors: Huang Lingcao
+email:huanglingcao@gmail.com
+add time: 16 February, 2021
+"""
+
+import os, sys
+code_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+sys.path.insert(0, code_dir)
+import parameters
+import basic_src.io_function as io_function
+import basic_src.basic as basic
+
+import workflow.postProcess as postProcess
+
+# add attributes to shapefile
+add_attributes_script = os.path.join(code_dir, 'datasets', 'get_polygon_attributes.py')
+
+def main():
+
+    area_ini_list =['area_Willow_River.ini', 'area_Banks_east_nirGB.ini', 'area_Ellesmere_Island_nirGB.ini']
+    for area_ini in area_ini_list:
+        ground_truth_shp = parameters.get_file_path_parameters(area_ini, 'validation_shape')
+
+        # save to current folder
+        save_info_shp = io_function.get_name_by_adding_tail(ground_truth_shp,'post')
+        save_info_shp = os.path.join(os.getcwd(), os.path.basename(save_info_shp))
+        if os.path.isfile(save_info_shp):
+            basic.outputlogMessage('%s already exist, skip %s'%(save_info_shp, area_ini))
+            continue
+        postProcess.add_polygon_attributes(add_attributes_script,ground_truth_shp,save_info_shp,area_ini,area_ini)
+
+
+if __name__ == '__main__':
+    main()
+    pass
+
+
+
+
+
