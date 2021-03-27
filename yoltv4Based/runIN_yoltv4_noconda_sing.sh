6676a457b129bed2c031fc334f28d4c916b9fbf5
Lingcao Huang <huanglingcao@gmail.com>
2021-03-27 16:19:41 -0600

    env setting

--- yoltv4Based/runIN_yoltv4_noconda_sing.sh
+++ yoltv4Based/runIN_yoltv4_noconda_sing.sh
@@ -18,15 +18,14 @@ yoltv4=~/programs/miniconda3/envs/yoltv4
 
 #export SINGULARITY_BINDPATH=/500G:/500G,/DATA1:/DATA1,/home/hlc:/home/hlc
 
-# HOME will be overwrite by the value in host machine, no matter what I did, it still be overwritten.
-# LD_LIBRARY_PATH has been overwrite as well, but if we unset LD_LIBRARY_PATH or use --cleanenv, then the problem is solved
+#SINGULARITYENV_LD_LIBRARY_PATH=/usr/lib64:${yoltv4}/lib:${LD_LIBRARY_PATH} \
 
-#SINGULARITYENV_LD_LIBRARY_PATH=/usr/lib64:${env_home}/programs/cuda-10.0/lib64:${env_home}/programs/cuDNN_7.4_cuda10/cuda/lib64:/usr/lib64:$LD_LIBRARY_PATH \
+# becasue SINGULARITYENV_PATH will overwrite some env inside the container, so we set them manually
+PATH_IN_SINGULARITY=/usr/local/cuda/bin:/usr/sbin:/usr/bin:/sbin:/bin
+#LIBRARY_PATH_IN_SINGULARITY=
 
-#SINGULARITYENV_LD_LIBRARY_PATH=${env_home}/programs/miniconda3/lib:$LD_LIBRARY_PATH \
 # set environment
 SINGULARITYENV_TZ=America/Denver \
-SINGULARITYENV_PATH=/bin:${yoltv4}/bin:$PATH \
-SINGULARITYENV_LD_LIBRARY_PATH=/usr/lib64:${yoltv4}/lib:$LD_LIBRARY_PATH \
+SINGULARITYENV_PATH=${yoltv4}/bin:${PATH_IN_SINGULARITY} \
 SINGULARITYENV_GDAL_DATA=${yoltv4}/share/gdal \
 singularity exec --nv ${sing_img} ${exe_script}
