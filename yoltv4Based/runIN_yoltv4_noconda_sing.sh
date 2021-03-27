9eca5dfef6f44a3fc25edb4645bba9f781675f03
Lingcao Huang <huanglingcao@gmail.com>
2021-03-27 16:33:42 -0600

    change path

--- yoltv4Based/runIN_yoltv4_noconda_sing.sh
+++ yoltv4Based/runIN_yoltv4_noconda_sing.sh
@@ -6,6 +6,8 @@
 #email:huanglingcao@gmail.com
 #add time: 27 March, 2021
 
+# need singularity >= 3.6
+
 # running script
 exe_script=./exe.sh
 
@@ -16,16 +18,16 @@ sing_img=${sing_dir}/programs/yoltv4_noconda.sif
 
 yoltv4=~/programs/miniconda3/envs/yoltv4
 
-#export SINGULARITY_BINDPATH=/500G:/500G,/DATA1:/DATA1,/home/hlc:/home/hlc
+# more about the environment setting: https://sylabs.io/guides/3.7/user-guide/environment_and_metadata.html
 
+#export SINGULARITY_BINDPATH=/500G:/500G,/DATA1:/DATA1,/home/hlc:/home/hlc
 #SINGULARITYENV_LD_LIBRARY_PATH=/usr/lib64:${yoltv4}/lib:${LD_LIBRARY_PATH} \
 
-# becasue SINGULARITYENV_PATH will overwrite some env inside the container, so we set them manually
-PATH_IN_SINGULARITY=/usr/local/cuda/bin:/usr/sbin:/usr/bin:/sbin:/bin
-#LIBRARY_PATH_IN_SINGULARITY=
+# use SINGULARITYENV_APPEND_PATH or SINGULARITYENV_PREPEND_PATH to add path on the host machine
+export SINGULARITYENV_PREPEND_PATH=${yoltv4}
+
 
 # set environment
 SINGULARITYENV_TZ=America/Denver \
-SINGULARITYENV_PATH=${yoltv4}/bin:${PATH_IN_SINGULARITY} \
 SINGULARITYENV_GDAL_DATA=${yoltv4}/share/gdal \
 singularity exec --nv ${sing_img} ${exe_script}
