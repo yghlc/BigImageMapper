4e2fc6cc4d2441ba16f59e0b33b83797b2c3b3ac
Lingcao Huang <huanglingcao@gmail.com>
2021-04-02 13:52:58 -0600

    multiple GPUs

--- yolov4_dir/exe.sh
+++ yolov4_dir/exe.sh
@@ -20,5 +20,8 @@ set -eE -o functrace
 # -map flag overlays mean average precision on chart to see how accuracy of your model is, only add map flag if you have a validation dataset
 
 #./darknet detector train <path to obj.data> <path to custom config> yolov4.conv.137 -dont_show -map
-darknet detector train obj.data yolov4_obj.cfg yolov4.conv.137 -dont_show -map
+#darknet detector train obj.data yolov4_obj.cfg yolov4.conv.137 -dont_show -map
 
+# stop 1-gpu training, then start multiple GPU trianing.
+# need to check if get Nan, if yes, need to change learning rates and burn in https://github.com/AlexeyAB/darknet#how-to-train-with-multi-gpu
+darknet detector train obj.data yolov4_obj.cfg train_backup/yolov4_obj_1gpu.weights -gpus 0,1,2,3
