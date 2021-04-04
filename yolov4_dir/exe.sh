b92bbefc01582885ff0dfb9be10fbb05546dff78
Lingcao Huang <huanglingcao@gmail.com>
2021-04-03 20:47:28 -0600

    save examples of prediction

--- yolov4_dir/exe.sh
+++ yolov4_dir/exe.sh
@@ -22,6 +22,23 @@ set -eE -o functrace
 #./darknet detector train <path to obj.data> <path to custom config> yolov4.conv.137 -dont_show -map
 #darknet detector train obj.data yolov4_obj.cfg yolov4.conv.137 -dont_show -map
 
+# resume training
+#darknet detector train obj.data yolov4_obj.cfg train_backup/yolov4_obj_last.weights -dont_show -map
+
 # stop 1-gpu training, then start multiple GPU trianing.
 # need to check if get Nan, if yes, need to change learning rates and burn in https://github.com/AlexeyAB/darknet#how-to-train-with-multi-gpu
 darknet detector train obj.data yolov4_obj.cfg train_backup/yolov4_obj_1gpu.weights -gpus 0,1,2,3  -dont_show -map
+
+
+
+## prediction
+#./darknet detector test <path to .data file> <path to config> <path to weights> <path to image>
+
+# #it wil save to "predictions.jpg" or show it if there is display env
+#darknet detector test obj.data yolov4_obj.cfg train_backup/yolov4_obj_5000.weights data/test/c7df5232291486a3.jpg
+#darknet detector test obj.data yolov4_obj.cfg train_backup/yolov4_obj_5000.weights data/test/53386dae3cf13cc3.jpg -dont_show  -ext_output
+
+## multiple inputs and save results to result.json
+#darknet detector test obj.data yolov4_obj.cfg train_backup/yolov4_obj_5000.weights -dont_show  -ext_output -out result.json < test_images.txt
+# save result to txt (same things previously on the screen)
+#darknet detector test obj.data yolov4_obj.cfg train_backup/yolov4_obj_5000.weights -dont_show  -ext_output < test_images.txt > result.txt
