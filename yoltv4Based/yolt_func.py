766c72c43fe7a53b7c73ecea5e797bd52296178e
Lingcao Huang <huanglingcao@gmail.com>
2021-04-08 15:20:02 -0600

    copy a function

--- yoltv4Based/yolt_func.py
+++ yoltv4Based/yolt_func.py
@@ -8,6 +8,8 @@ email:huanglingcao@gmail.com
 add time: 04 April, 2021
 """
 
+import numpy as np
+
 # https://github.com/CosmiQ/simrdwn/blob/master/simrdwn/data_prep/yolt_data_prep_funcs.py
 def convert(size, box):
     '''Input = image size: (w,h), box: [x0, x1, y0, y1]'''
@@ -41,3 +43,97 @@ def convert_reverse(size, box):
     y0, y1 = ymid - h0/2., ymid + h0/2.
 
     return [x0, x1, y0, y1]
+
+
+# https://github.com/avanetten/simrdwn/blob/master/simrdwn/core/post_process.py
+###############################################################################
+def non_max_suppression(boxes, probs=[], overlapThresh=0.5):
+    """
+    Apply non-max suppression.
+    Adapted from:
+    http://www.pyimagesearch.com/2015/02/16/faster-non-maximum-suppression-python/
+    Malisiewicz et al.
+    see modular_sliding_window.py, functions non_max_suppression, \
+            non_max_supression_rot
+    Another option:
+        https://github.com/YunYang1994/tensorflow-yolov3/blob/master/core/utils.py
+    Arguments
+    ---------
+    boxes : np.array
+        Prediction boxes with the format: [[xmin, ymin, xmax, ymax], [...] ]
+    probs : np.array
+        Array of prediction scores or probabilities.  If [], ignore.  If not
+        [], sort boxes by probability prior to applying non-max suppression.
+        Defaults to ``[]``.
+    overlapThresh : float
+        minimum IOU overlap to retain.  Defaults to ``0.5``.
+    Returns
+    -------
+    pick : np.array
+        Array of indices to keep
+    """
+
+    print("Executing non-max suppression...")
+    len_init = len(boxes)
+
+    # if there are no boxes, return an empty list
+    if len(boxes) == 0:
+        return [], [], []
+
+    # boxes_tot = boxes  # np.asarray(boxes)
+    boxes = np.asarray([b[:4] for b in boxes])
+    # if the bounding boxes integers, convert them to floats --
+    # this is important since we'll be doing a bunch of divisions
+    if boxes.dtype.kind == "i":
+        boxes = boxes.astype("float")
+
+    # initialize the list of picked indexes
+    pick = []
+
+    # grab the coordinates of the bounding boxes
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+    # compute the area of the bounding boxes
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+
+    # sort the boxes by the bottom-right y-coordinate of the bounding box
+    if len(probs) == 0:
+        idxs = np.argsort(y2)
+    # sort boxes by the highest prob (descending order)
+    else:
+        idxs = np.argsort(probs)[::-1]
+
+    # keep looping while some indexes still remain in the indexes
+    # list
+    while len(idxs) > 0:
+        # grab the last index in the indexes list and add the
+        # index value to the list of picked indexes
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+
+        # find the largest (x, y) coordinates for the start of
+        # the bounding box and the smallest (x, y) coordinates
+        # for the end of the bounding box
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+        # compute the width and height of the bounding box
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+
+        # compute the ratio of overlap
+        overlap = (w * h) / area[idxs[:last]]
+
+        # delete all indexes from the index list that have
+        idxs = np.delete(
+            idxs,
+            np.concatenate(([last], np.where(overlap > overlapThresh)[0])))
+
+    print("  non-max suppression init boxes:", len_init)
+    print("  non-max suppression final boxes:", len(pick))
+    return pick
