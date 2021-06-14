f7c2ec599fd416de18c6979bb929d1bbedac528f
Lingcao Huang <huanglingcao@gmail.com>
2021-06-14 09:37:03 -0600

    add more

--- flooding_mapping/bash_region_parafile.sh
+++ flooding_mapping/bash_region_parafile.sh
@@ -20,11 +20,30 @@ function new_ini(){
 }
 
 rm region_ini_files.txt || true
+rm *.ini || true
+
+ref_ini=~/Data/flooding_area/automapping/houston_deeplabV3+_1/area_Houston.ini
 
 region=Houston
-ref_ini=~/Data/flooding_area/automapping/houston_deeplabV3+_1/area_Houston.ini
 dir=~/Bhaltos2/lingcaoHuang/flooding_area/Houston/Houston_SAR_GRD_FLOAT_gee/S1_Houston_prj_8bit_select
 new_ini ${dir} ${ref_ini} ${region} 
+
+
+region=Goalpara
+dir=~/Bhaltos2/lingcaoHuang/flooding_area/Goalpara/Goalpara_power_transform_prj_8bit
+new_ini ${dir} ${ref_ini} ${region}
+
+
+region=Vadodara_lower
+dir=~/Bhaltos2/lingcaoHuang/flooding_area/Vadodara/Vadodara_2020_lower_power_transform_prj_8bit
+new_ini ${dir} ${ref_ini} ${region}
+
+
+region=Vadodara_upper
+dir=~/Bhaltos2/lingcaoHuang/flooding_area/Vadodara/Vadodara_2020_upper_power_transform_prj_8bit
+new_ini ${dir} ${ref_ini} ${region}
+
+
 
 
 
