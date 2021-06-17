de6aa5fda0cfbfca1effde049b900b374b83bcf0
Lingcao Huang <huanglingcao@gmail.com>
2021-06-17 08:32:43 -0600

    add

--- flooding_mapping/bash_region_parafile_binary.sh
+++ flooding_mapping/bash_region_parafile_binary.sh
@@ -39,7 +39,7 @@ dir=~/Bhaltos2/lingcaoHuang/flooding_area/Vadodara/Vadodara_2020_lower_power_tra
 new_ini ${dir} ${ref_ini} ${region}
 
 
-region=Vadodara_upper
+region=Vadodara_upperu
 dir=~/Bhaltos2/lingcaoHuang/flooding_area/Vadodara/Vadodara_2020_upper_binary_255
 new_ini ${dir} ${ref_ini} ${region}
 
