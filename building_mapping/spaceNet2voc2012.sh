cb58ecf1d4a24d7e631547d9dcd39026dd0cd117
Lingcao Huang <huanglingcao@gmail.com>
2021-07-01 14:52:47 -0600

    clean

--- building_mapping/spaceNet2voc2012.sh
+++ building_mapping/spaceNet2voc2012.sh
@@ -29,37 +29,4 @@ for AOI in $(ls -d ${root}/*train*/*Train*); do
 done
 
 
-exit
-
-
-
-# remove previous success_save.txt file
-rm success_save.txt
-
-#echo ${AOIs} ${AOI_3} ${AOI_4} ${AOI_5}
-for AOI in ${AOI_2} ${AOI_3} ${AOI_4} ${AOI_5}
-do
-    echo training data dir: $spacenet_root/$AOI
-
-    #using createDataSpaceNet.py to convert spaceNet file to PASCALVOC2012 format
-#    python python/createDataSpaceNet.py /path/to/spacenet_sample/AOI_2_Vegas_Train/ \
-#           --srcImageryDirectory RGB-PanSharpen
-#           --outputDirectory /path/to/spacenet_sample/annotations/ \
-#           --annotationType PASCALVOC2012 \
-#           --imgSizePix 400
-
-training_data_root=${spacenet_root}/${AOI}
-outputDirectory=${output_root}/${AOI}
-echo ${training_data_root}
-echo ${outputDirectory}
-
-#using createDataSpaceNet.py to convert spaceNet file to PASCALVOC2012 format
-python ${python_script} ${training_data_root} --convertTo8Bit --trainTestSplit 0.8 --srcImageryDirectory RGB-PanSharpen --outputDirectory ${outputDirectory} --annotationType PASCALVOC2012
-
-#using createDataSpaceNet.py to convert spaceNet file to SBD format
-#python ${python_script} ${training_data_root} --convertTo8Bit --trainTestSplit 0.8 --srcImageryDirectory RGB-PanSharpen --outputDirectory ${outputDirectory} --annotationType SBD
-
-
-done
-
 
