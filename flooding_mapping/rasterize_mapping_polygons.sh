30782e0211c80d4b76f1f705e71053fe32de5a97
Lingcao Huang <huanglingcao@gmail.com>
2021-07-04 10:42:54 -0600

    other region

--- flooding_mapping/rasterize_mapping_polygons.sh
+++ flooding_mapping/rasterize_mapping_polygons.sh
@@ -7,7 +7,7 @@
 #add time: 3 July, 2021
 
 # Exit immediately if a command exits with a non-zero status. E: error trace
-set -eE -o functrace
+#set -eE -o functrace
 
 shp_dir=~/Data/flooding_area/automapping/houston_deeplabV3+_1/result_backup
 out_dir=~/Data/flooding_area/mapping_polygons_rasters
@@ -20,15 +20,16 @@ py=~/codes/PycharmProjects/Landuse_DL/datasets/rasterize_polygons.py
 function rasterize_map() {
   folder=$1
   ref_tif_dir=$2
+  date_pos=$3
   for shp in $(ls ${shp_dir}/${folder}/*/*.shp); do
     echo $shp
     save_dir=${out_dir}/$folder
     name=$(basename $shp)
-    date_str=$(echo $name | cut -d "_"  -f 3)
+    date_str=$(echo $name | cut -d "_"  -f ${date_pos})
 
     ref_raster=$(ls ${ref_tif_dir}/*${date_str}*.tif)
     echo $ref_raster
-    if [ ! -f ${ref_raster} ]; then
+    if [ -z "$ref_raster"  ]; then # ! -f ${ref_raster
       echo "error, ${ref_raster} not exist, skip"
       continue
     fi
@@ -36,9 +37,23 @@ function rasterize_map() {
     ${py} -r ${ref_raster} -o ${save_dir} -b 1 ${shp}
   done
 }
-area_exp=exp1_grd_Goalpara
-ref_tif_dir=~/Data/flooding_area/Goalpara/Goalpara_power_transform_prj_8bit
-rasterize_map ${area_exp} ${ref_tif_dir}
+#area_exp=exp1_grd_Goalpara
+#ref_tif_dir=~/Data/flooding_area/Goalpara/Goalpara_power_transform_prj_8bit
+#rasterize_map ${area_exp} ${ref_tif_dir} 3
+
+
+#area_exp=exp2_binary_Goalpara
+#ref_tif_dir=~/Data/flooding_area/Goalpara/Goalpara_power_transform_prj_8bit
+#rasterize_map ${area_exp} ${ref_tif_dir} 4
+
+
+#area_exp=exp1_grd_Houston
+#ref_tif_dir=~/Data/flooding_area/Houston/Houston_mosaic
+#rasterize_map ${area_exp} ${ref_tif_dir} 3
+
+
+
+
 
 
 
