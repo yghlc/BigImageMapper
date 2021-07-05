69fd90f32a51ea5042362b664b5afb3d0eeaaf21
Lingcao Huang <huanglingcao@gmail.com>
2021-07-05 13:36:52 -0600

    Goalpara region

--- flooding_mapping/flooding_proc_accuracy.py
+++ flooding_mapping/flooding_proc_accuracy.py
@@ -87,6 +87,28 @@ def mask_by_surface_water(map_raster, surface_water_crop):
     if raster_io.save_numpy_array_to_rasterfile(map_array_2d,save_mask_result,map_raster, compress='lzw',tiled='Yes',bigtiff='if_safer'):
         return save_mask_result
 
+def mask_by_elevation(map_raster_path, elevation_crop_path, threashold):
+
+    # save mask result to current folder
+    save_mask_result = io_function.get_name_by_adding_tail(os.path.basename(map_raster_path),'DEMMask')
+    if os.path.isfile(save_mask_result):
+        print('warning, %s exists'%save_mask_result)
+        return save_mask_result
+
+    # read
+    map_array_2d,nodata = raster_io.read_raster_one_band_np(map_raster_path)
+    dem_array_2d,_ = raster_io.read_raster_one_band_np(elevation_crop_path)
+
+    print(map_array_2d.shape)
+    if map_array_2d.shape != dem_array_2d.shape:
+        raise ValueError('size inconsistent: %s and %s'%(str(map_array_2d.shape), str(dem_array_2d.shape)))
+
+    # mask out pixel with high elevation
+    map_array_2d[ dem_array_2d > threashold ] = 0
+
+    if raster_io.save_numpy_array_to_rasterfile(map_array_2d,save_mask_result,map_raster_path, compress='lzw',tiled='Yes',bigtiff='if_safer'):
+        return save_mask_result
+
 
 def post_processing_Houston():
     # mapping raster
@@ -121,15 +143,19 @@ def post_processing_Houston():
 def post_processing_Goapara():
 
     # mapping raster
+    # these mapping raster have the same width and height: Size is 28825, 21970
     dl_map_20200516 = os.path.join(data_dir,'mapping_polygons_rasters/exp1_grd_Goalpara/Goalpara_SAR_20200516_houston_deeplabV3+_1_exp1_post_1_label.tif')
-    dl_map_20200516 = os.path.join(data_dir,'mapping_polygons_rasters/exp1_grd_Goalpara/Goalpara_SAR_20200528_houston_deeplabV3+_1_exp1_post_1_label.tif')
-    dl_map_20200516 = os.path.join(data_dir,'mapping_polygons_rasters/exp1_grd_Goalpara/Goalpara_SAR_20200609_houston_deeplabV3+_1_exp1_post_1_label.tif')
-    dl_map_20200516 = os.path.join(data_dir,'mapping_polygons_rasters/exp1_grd_Goalpara/Goalpara_SAR_20200621_houston_deeplabV3+_1_exp1_post_1_label.tif')
-    dl_map_20200516 = os.path.join(data_dir,'mapping_polygons_rasters/exp1_grd_Goalpara/Goalpara_SAR_20200703_houston_deeplabV3+_1_exp1_post_1_label.tif')
-    dl_map_20200516 = os.path.join(data_dir,'mapping_polygons_rasters/exp1_grd_Goalpara/Goalpara_SAR_20200715_houston_deeplabV3+_1_exp1_post_1_label.tif')
-    dl_map_20200516 = os.path.join(data_dir,'mapping_polygons_rasters/exp1_grd_Goalpara/Goalpara_SAR_20200727_houston_deeplabV3+_1_exp1_post_1_label.tif')
-    dl_map_20200516 = os.path.join(data_dir,'mapping_polygons_rasters/exp1_grd_Goalpara/Goalpara_SAR_20200808_houston_deeplabV3+_1_exp1_post_1_label.tif')
-    dl_map_20200516 = os.path.join(data_dir,'mapping_polygons_rasters/exp1_grd_Goalpara/Goalpara_SAR_20200820_houston_deeplabV3+_1_exp1_post_1_label.tif')
+    # dl_map_20200528 = os.path.join(data_dir,'mapping_polygons_rasters/exp1_grd_Goalpara/Goalpara_SAR_20200528_houston_deeplabV3+_1_exp1_post_1_label.tif')
+    # dl_map_20200609 = os.path.join(data_dir,'mapping_polygons_rasters/exp1_grd_Goalpara/Goalpara_SAR_20200609_houston_deeplabV3+_1_exp1_post_1_label.tif')
+    # dl_map_20200621 = os.path.join(data_dir,'mapping_polygons_rasters/exp1_grd_Goalpara/Goalpara_SAR_20200621_houston_deeplabV3+_1_exp1_post_1_label.tif')
+    # dl_map_20200703 = os.path.join(data_dir,'mapping_polygons_rasters/exp1_grd_Goalpara/Goalpara_SAR_20200703_houston_deeplabV3+_1_exp1_post_1_label.tif')
+    # dl_map_20200715 = os.path.join(data_dir,'mapping_polygons_rasters/exp1_grd_Goalpara/Goalpara_SAR_20200715_houston_deeplabV3+_1_exp1_post_1_label.tif')
+    # dl_map_20200727 = os.path.join(data_dir,'mapping_polygons_rasters/exp1_grd_Goalpara/Goalpara_SAR_20200727_houston_deeplabV3+_1_exp1_post_1_label.tif')
+    # dl_map_20200808 = os.path.join(data_dir,'mapping_polygons_rasters/exp1_grd_Goalpara/Goalpara_SAR_20200808_houston_deeplabV3+_1_exp1_post_1_label.tif')
+    # dl_map_20200820 = os.path.join(data_dir,'mapping_polygons_rasters/exp1_grd_Goalpara/Goalpara_SAR_20200820_houston_deeplabV3+_1_exp1_post_1_label.tif')
+
+    # get dl map results (include dl_map_20200516)
+    dl_map_results = io_function.get_file_list_by_ext('.tif',os.path.join(data_dir,'mapping_polygons_rasters/exp1_grd_Goalpara'), bsub_folder=False)
 
     # other map results (consider as ground truth)
     modis_map_20200705 = os.path.join(data_dir,'flooding_rasters_other_results/Goapara/20200705MODIS_region_prj_label.tif')
@@ -141,18 +167,33 @@ def post_processing_Goapara():
     dem_path = os.path.join(data_dir, 'extent_UTM46N_Goalpara/extent_80_90E_30N_v1_3_2020.tif')
 
     # resample and crop to the same resolution and extent
+    surface_water_crop = resample_crop_raster(dl_map_20200516,surface_water)
+    dem_crop = resample_crop_raster(dl_map_20200516,dem_path)
 
+    # mask by using the global surface water
+    dl_map_results_watermask = [mask_by_surface_water(item,surface_water_crop) for item in dl_map_results ]
+    modis_map_20200705_watermask = mask_by_surface_water(modis_map_20200705, surface_water_crop)
+    dl_map_results = dl_map_results_watermask
+
+    # post-processing by using the DEM
+    elevation_thr = 500
+    dl_map_results_watermask = [mask_by_elevation(item, dem_crop,elevation_thr) for item in dl_map_results]
 
     # post-processing the multi-temporal results
 
+    # calculate accuracy for each dl mapping results
+    for idx, dl_map in enumerate(dl_map_results_watermask):
+        print(idx, dl_map)
+        classify_assess.pixel_accuracy_assessment(modis_map_20200705_watermask,dl_map)
 
-    # mask by using the global surface water
 
     pass
 
 def main():
 
-    post_processing_Houston()
+    # post_processing_Houston()
+
+    post_processing_Goapara()
 
 
     pass
