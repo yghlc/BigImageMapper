4a8e56423b9c2cd1a9184aff31490eff12d98099
Lingcao Huang <huanglingcao@gmail.com>
2021-07-12 10:11:43 -0600

    plot more

--- LCLUC_mapping/gmt_mapBiomas_LCLU.sh
+++ LCLUC_mapping/gmt_mapBiomas_LCLU.sh
@@ -11,17 +11,27 @@
 #add time: 11 July, 2021
 
 img_dir=~/Data/LandCover_LandUse_Change/LCLUC_MapBiomas_Gabriel
-shp_dir=~/Data/LandCover_LandUse_Change/Potential_areas_for_preliminary_study/areas_extent
+#shp_dir=~/Data/LandCover_LandUse_Change/Potential_areas_for_preliminary_study/areas_extent
 
-img_2019=${img_dir}/COLECAO_5_DOWNLOADS_COLECOES_ANUAL_2019_merge_prj_crop.tif
+## for area1
+#xmin=256854
+#xmax=270842
+#ymin=6836773
+#ymax=6849079
 
-# for area1
-#area1_shp=${shp_dir}/study_area1.shp
-# get sub image using gdal_translate
-xmin=256854
-xmax=270842
-ymin=6836773
-ymax=6849079
+
+# for dam1_surrounding
+xmin=-3756
+xmax=18132
+ymin=6713740
+ymax=6731993
+
+
+## for dam2_surrounding
+#xmin=-25190
+#xmax=-126
+#ymin=6634467
+#ymax=6654893
 
 
 function crop_img(){
@@ -30,6 +40,9 @@ function crop_img(){
     # crop
     gdal_translate  -of NetCDF -projwin ${xmin} ${ymax} ${xmax} ${ymin} $in ${out}
 }
+
+# make a color table, then modify it
+# #gmt makecpt -Cbatlow -T1/41/1 -D > color.cpt
 
 #width=$(expr ${xmax} - ${xmin})
 width=12c  # 5 cm
@@ -58,31 +71,30 @@ function plot_LCLU_map(){
     gmt basemap -Ln0.8/0.12+w2000+lm --FONT_ANNOT_PRIMARY=10p,Helvetica,red --MAP_SCALE_HEIGHT=5p --MAP_TICK_PEN_PRIMARY=2p,red
 
     # add legend
-#    gmt legend -Dn0.9/0.12
+#    gmt legend -Dn0.9/0.12  #${year}.nc
 
     # draw colorbar
     gmt colorbar -CcolorMapBiomas.cpt -B
 
 
-  gmt end show
+  gmt end #show
 
 }
 
-#for year in $(seq 1985 2019); do
-#  echo $year
-#  map_tif=${img_dir}/COLECAO_5_DOWNLOADS_COLECOES_ANUAL_${year}_merge_prj_crop.tif
-#
+for year in $(seq 1985 2019); do
+  echo $year
+  map_tif=${img_dir}/COLECAO_5_DOWNLOADS_COLECOES_ANUAL_${year}_merge_prj_crop.tif
+
 #  plot_LCLU_map ${map_tif} $year area1
-#
-#done
-
-year=2019
-map_tif=${img_dir}/COLECAO_5_DOWNLOADS_COLECOES_ANUAL_${year}_merge_prj_crop.tif
-plot_LCLU_map ${map_tif} $year area1
-
-#gmt makecpt -Cbatlow -T1/41/1 -D > color.cpt
+  plot_LCLU_map ${map_tif} $year dam1_surr
+#  plot_LCLU_map ${map_tif} $year dam2_surr
 
 
+done
+
+#year=2019
+#map_tif=${img_dir}/COLECAO_5_DOWNLOADS_COLECOES_ANUAL_${year}_merge_prj_crop.tif
+#plot_LCLU_map ${map_tif} $year area1
 
 
 rm *.nc
