da9ed05a175418b41af71a386f086c49afa5a05f
Lingcao Huang <hlchuang@126.com>
2021-10-09 10:24:58 -0600

    image tranlation

--- workflow/image_translation_gan.py
+++ workflow/image_translation_gan.py
@@ -32,14 +32,29 @@ def CUT_gan_is_ready_to_train(working_folder):
 
 def train_CUT_gan(python_path, train_script,gan_para_file,gpu_ids):
 
-    
+    if os.path.isfile('train.txt_done'):
+        basic.outputlogMessage('training of GAN in %s has completed previoulsy, please remove it if necessary'%os.getcwd())
+        return True
+
+    time0 = time.time()
+    train_tile_width = parameters.get_digit_parameters(gan_para_file,'train_tile_width','int')
+    train_tile_height = parameters.get_digit_parameters(gan_para_file,'train_tile_height','int')
+    train_overlay_x = parameters.get_digit_parameters(gan_para_file,'train_overlay_x','int')
+    train_overlay_y = parameters.get_digit_parameters(gan_para_file,'train_overlay_y','int')
+
+    folder=os.path.basename(os.getcwd())
 
     command_string = python_path + ' '  +  train_script \
-                + ' os.chdir(current_dir)='+para_file \
-                + ' --inf_list_file='+inf_list_file \
-                + ' --inf_batch_size='+str(inf_batch_size) \
-                + ' --inf_output_dir='+save_dir \
-                + ' --frozen_graph_path='+frozen_graph_path
+                + ' --dataset_mode '+'satelliteimage' \
+                + ' --image_A_dir_txt ' + 'image_A_list.txt' \
+                + ' --image_B_dir_txt ' + 'image_B_list.txt' \
+                + ' --tile_width ' + str(train_tile_width) \
+                + ' --tile_height ' + str(train_tile_height) \
+                + ' --overlay_x ' + str(train_overlay_x) \
+                + ' --overlay_y ' + str(train_overlay_y)  \
+                + ' --display_env ' + folder  \
+                + ' --continue_train '  \
+                + ' --gpu_ids ' + '.'.join([str(item) for item in gpu_ids]) 
     # status, result = basic.exec_command_string(command_string)  # this will wait command finished
     # os.system(command_string + "&")  # don't know when it finished
     res = os.system(command_string)  # this work
@@ -48,16 +63,51 @@ def train_CUT_gan(python_path, train_script,gan_para_file,gpu_ids):
         sys.exit(1)
 
     duration = time.time() - time0
-    os.system('echo "$(date): time cost of inference for image in %s: %.2f seconds">>"time_cost.txt"' % (inf_list_file, duration))
-    # write a file to indicate that the prediction has done.
-    os.system('echo %s > %s_done'%(inf_list_file,inf_list_file))
+    os.system('echo "$(date): time cost of training a GAN : %.2f seconds">>"time_cost.txt"' % (duration))
+    # write a file to indicate that the training has completed.
+    os.system('echo done > train.txt_done')
 
-    
-    pass
+    return True
 
-def generate_image_CUT(python_path, generate_script):
+def generate_image_CUT(python_path, generate_script, gan_para_file, gpu_ids, image_list, save_folder):
 
-    pass
+    if os.path.isfile('generate.txt_done'):
+        basic.outputlogMessage('generate of new images using GAN in %s has completed previoulsy, please remove them if necessary'%os.getcwd())
+        return True
+
+    time0 = time.time()
+    generate_tile_width = parameters.get_digit_parameters(gan_para_file,'generate_tile_width','int')
+    generate_tile_height = parameters.get_digit_parameters(gan_para_file,'generate_tile_height','int')
+    generate_overlay_x = parameters.get_digit_parameters(gan_para_file,'generate_overlay_x','int')
+    generate_overlay_y = parameters.get_digit_parameters(gan_para_file,'generate_overlay_y','int')
+
+    folder=os.path.basename(os.getcwd())
+    img_list_txt = 'image_to_generate_list.txt'
+    io_function.save_list_to_txt(img_list_txt,image_list)
+
+    command_string = python_path + ' '  +  generate_script \
+                + ' --dataset_mode '+'satelliteimage' \
+                + ' --image_A_dir_txt ' + img_list_txt \
+                + ' --tile_width ' + str(generate_tile_width) \
+                + ' --tile_height ' + str(generate_tile_height) \
+                + ' --overlay_x ' + str(generate_overlay_x) \
+                + ' --overlay_y ' + str(generate_overlay_y)  \
+                + ' --results_dir ' + save_folder  \
+                + ' --gpu_ids ' + '.'.join([str(item) for item in gpu_ids]) 
+    # status, result = basic.exec_command_string(command_string)  # this will wait command finished
+    # os.system(command_string + "&")  # don't know when it finished
+    res = os.system(command_string)  # this work
+    # print('command_string deeplab_inf_script: res',res)
+    if res != 0:
+        sys.exit(1)
+
+    duration = time.time() - time0
+    os.system('echo "$(date): time cost of generate images using a GAN : %.2f seconds">>"time_cost.txt"' % (duration))
+    # write a file to indicate that the process has completed.
+    os.system('echo done > generate.txt_done')
+
+    return True
+
 
 def image_translate_train_generate_one_domain(gan_working_dir, gan_para_file, area_ini, gpu_ids, domainB_imgList):
 
@@ -68,8 +118,6 @@ def image_translate_train_generate_one_domain(gan_working_dir, gan_para_file, ar
     if os.path.isfile(sub_img_label_txt) is False:
         raise IOError('%s not in the current folder, please get subImages first' % sub_img_label_txt)
 
-    
-    os.chdir(gan_working_dir)
     # prepare image list of domain A
     # what if the size of some images are not fit with CUT input?
     domain_A_images = []
@@ -78,13 +126,17 @@ def image_translate_train_generate_one_domain(gan_working_dir, gan_para_file, ar
         line_list = [name.strip() for name in txt_obj.readlines()]
         for line in line_list:
             sub_image, sub_label = line.split(':')
-            domain_A_images.append(sub_image)
-            domain_A_labels.append(sub_label)
-    io_function.save_list_to_txt(domain_A_images,'image_A_list.txt')
+            domain_A_images.append(os.path.join(current_dir,sub_image))
+            domain_A_labels.append(os.path.join(current_dir,sub_label))
+
+    
+    os.chdir(gan_working_dir)
+
+    io_function.save_list_to_txt('image_A_list.txt',domain_A_images)
 
     # read target images, that will consider as target domains
     # what if there are too many images in domain B?
-    io_function.save_list_to_txt(domainB_imgList,'image_B_list.txt')
+    io_function.save_list_to_txt('image_B_list.txt',domainB_imgList)
 
     
     gan_python = parameters.get_file_path_parameters(gan_para_file,'python')
@@ -92,14 +144,15 @@ def image_translate_train_generate_one_domain(gan_working_dir, gan_para_file, ar
     train_script = os.path.join(cut_dir, 'train.py')
     generate_script = os.path.join(cut_dir,'generate_image.py')
     # training of CUT 
-    if train_CUT_gan(gan_python,train_script) is False:
+    if train_CUT_gan(gan_python,train_script,gan_para_file,gpu_ids) is False:
         os.chdir(current_dir)
         return False
 
-    # genenerate image using CUT
-
-
-
+    # genenerate image using CUT, convert images in domain A to domain B
+    save_tran_img_folder = os.path.join(gan_working_dir,'subImages_translate')
+    if generate_image_CUT(gan_python, generate_script,gan_para_file,gpu_ids, domain_A_images, save_tran_img_folder) is False:
+        os.chdir(current_dir)
+        return False
 
 
     # change working directory back
@@ -121,6 +174,7 @@ def image_translate_train_generate_main(para_file, gpu_num):
         raise IOError('File %s not exists in current folder: %s'%(para_file, os.getcwd()))
 
     gan_para_file = parameters.get_string_parameters_None_if_absence(para_file, 'regions_n_setting_image_translation_ini')
+    gan_para_file = os.path.abspath(gan_para_file)  # change to absolute path, because later, we change folder
     if gan_para_file is None:
         print('regions_n_setting_image_translation_ini is not set, skip image translation using GAN')
         return None
@@ -131,11 +185,13 @@ def image_translate_train_generate_main(para_file, gpu_num):
     # get regions (equal to or subset of inference regions) need apply image translation
     multi_gan_regions = parameters.get_string_list_parameters(gan_para_file, 'regions_need_image_translation')
     gan_working_dir = parameters.get_string_parameters(gan_para_file, 'working_root')
+    gan_dir_pre_name = parameters.get_string_parameters(gan_para_file, 'gan_dir_pre_name')
 
     # loop each regions need image translation
     sub_tasks = []
     for area_idx, area_ini in enumerate(multi_gan_regions):
 
+        area_ini = os.path.abspath(area_ini)
         area_name = parameters.get_string_parameters(area_ini, 'area_name')
         area_remark = parameters.get_string_parameters(area_ini, 'area_remark')
         area_time = parameters.get_string_parameters(area_ini, 'area_time')
@@ -150,10 +206,10 @@ def image_translate_train_generate_main(para_file, gpu_num):
         if img_count < 1:
             raise ValueError('No image for image translation, please check inf_image_dir and inf_image_or_pattern in %s' % area_ini)
 
-        gan_project_save_dir = os.path.join(gan_working_dir, area_name + '_' + area_remark + '_' + area_time)
+        gan_project_save_dir = os.path.join(gan_working_dir, gan_dir_pre_name + '_' + area_name + '_' + area_remark + '_' + area_time)
 
         if os.path.isdir(gan_project_save_dir):
-            # TODO: check if results already exist
+            # check if results already exist
             pass
         else:
             io_function.mkdir(gan_project_save_dir)
@@ -183,7 +239,7 @@ def image_translate_train_generate_main(para_file, gpu_num):
                 continue
             # set only the first available visible
             gpuids.append(deviceIDs[0])
-            basic.outputlogMessage('%d:image translation for  %s on GPU %d of %s' % (area_idx, area_ini, gpuid, machine_name))
+            basic.outputlogMessage('%d:image translation for  %s on GPU %s of %s' % (area_idx, area_ini, str(gpuids), machine_name))
 
         # run image translation
 
