26ac1b378e019b1dcef11a084e812f38c27545b9
Lingcao Huang <huanglingcao@gmail.com>
2022-01-12 09:37:37 -0700

    add, placeholder for mmseg

---
+++ mmseg_dir/exe_mmseg.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+#introduction: Run the whole process of mapping landforms using mmseg(mmsegmentation)
+#
+#authors: Huang Lingcao
+#email:huanglingcao@gmail.com
+#add time: 11 January, 2022
+
+# Exit immediately if a command exits with a non-zero status. E: error trace
+set -eE -o functrace
+
+
+# set GPUs want to used (e.g. use GPU 0 & 1)
+export CUDA_VISIBLE_DEVICES=0,1
+# number of GPUs
+gpu_num=2
+
+# the main parameter files
+para_file=main_para.ini
+
+# Landuse_DL scripts dir
+eo_dir=~/codes/PycharmProjects/Landuse_DL
+
+################################################
+SECONDS=0
+# remove previous data or results if necessary
+#rm time_cost.txt || true
+${eo_dir}/workflow/remove_previous_data.py ${para_file}
+
+#extract sub_images based on the training polgyons
+${eo_dir}/workflow/get_sub_images_multi_regions.py ${para_file}
+
+# apply image translation using GAN 
+#${eo_dir}/workflow/image_translation_gan.py ${para_file} ${gpu_num}
+
+################################################
+## preparing training images.
+
+${eo_dir}/workflow/split_sub_images.py ${para_file}
+${eo_dir}/workflow/training_img_augment.py ${para_file}
+${eo_dir}/workflow/split_train_val.py ${para_file}
+
+
+duration=$SECONDS
+echo "$(date): time cost of preparing training data: ${duration} seconds">>"time_cost.txt"
+
+################################################
+## training
+
+${eo_dir}/mmseg_dir/mmseg_train.py ${para_file} ${gpu_num}
+
+################################################
+
+################################################
+## inference
+rm -r multi_inf_results || true
+${eo_dir}/mmseg_dir/mmseg_predict.py ${para_file}
+
+
+## post processing and copy results, inf_post_note indicate notes for inference and post-processing
+inf_post_note=1
+${eo_dir}/workflow/postProcess.py ${para_file}  ${inf_post_note}
+
+
+
+#################################################
+### conduct polygon-based change detection based on the multi-temporal mapping results
+#cd_code=~/codes/PycharmProjects/ChangeDet_DL
+#${cd_code}/thawSlumpChangeDet/polygons_cd_multi_exe.py ${para_file} ${test_name}
---
+++ mmseg_dir/mmseg_predict.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# Filename: mmseg_predict.py 
+"""
+introduction: run prediction of a deep learning model for semantic segmentation using MMSegmentation
+
+authors: Huang Lingcao
+email:huanglingcao@gmail.com
+add time: 11 January, 2022
+"""
+
+import os,sys
+from optparse import OptionParser
+
+def main(options, args):
+    pass
+
+
+if __name__ == '__main__':
+    pass
+
+
+
+
+
+
+
