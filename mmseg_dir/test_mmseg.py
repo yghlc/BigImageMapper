f3eecccde8412a7c5a48be6c38b0cbcb67568e3a
Lingcao Huang <huanglingcao@gmail.com>
2022-02-06 09:08:00 -0700

    run more test

--- mmseg_dir/test_mmseg.py
+++ mmseg_dir/test_mmseg.py
@@ -17,6 +17,7 @@ sys.path.insert(0, code_dir)
 import basic_src.io_function as io_function
 import parameters
 from mmseg_predict import predict_one_image_mmseg
+from workflow.postProcess import inf_results_to_shapefile
 
 # sys.setrecursionlimit(100)
 
@@ -41,14 +42,19 @@ def test_dataloader():
     # test, run in ~/Data/tmp_data/test_mmsegmentation/test_landuse_dl
     para_file = 'main_para.ini'
     set_pythonpath(para_file)
+    img_idx = 0
 
     image_path = os.path.expanduser('~/Data/Arctic/canada_arctic/Willow_River/Planet2020/20200818_mosaic_8bit_rgb.tif')
-    img_save_dir = 'predict_output'
+    img_save_dir = os.path.join('predict_output','I%d' % img_idx)
     io_function.mkdir(img_save_dir)
-    inf_list_file = '0.txt'
+    inf_list_file = os.path.join('predict_output','%d.txt'%img_idx)
     gpuid = None
     trained_model = 'exp18/latest.pth'
     predict_one_image_mmseg(para_file, image_path, img_save_dir, inf_list_file, gpuid, trained_model)
+
+    # curr_dir,img_idx, area_save_dir, test_id
+    curr_dir = os.getcwd()
+    inf_results_to_shapefile(curr_dir,img_idx,'predict_output',1)
 
 
 def main():
