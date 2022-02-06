55f4b0424b458e9891206647c846b8a62c725ab6
Lingcao Huang <huanglingcao@gmail.com>
2022-02-06 10:22:22 -0700

    bug fix

--- mmseg_dir/test_mmseg.py
+++ mmseg_dir/test_mmseg.py
@@ -23,7 +23,6 @@ from workflow.postProcess import inf_results_to_shapefile
 
 def set_pythonpath(para_file):
 
-    expr_name = parameters.get_string_parameters(para_file, 'expr_name')
     network_ini = parameters.get_string_parameters(para_file, 'network_setting_ini')
     mmseg_repo_dir = parameters.get_directory(network_ini, 'mmseg_repo_dir')
     mmseg_code_dir = osp.join(mmseg_repo_dir,'mmseg')
@@ -42,20 +41,21 @@ def test_dataloader():
     # test, run in ~/Data/tmp_data/test_mmsegmentation/test_landuse_dl
     para_file = 'main_para.ini'
     set_pythonpath(para_file)
+    expr_name = parameters.get_string_parameters(para_file, 'expr_name')
+    trained_model = '%s/latest.pth'%expr_name
 
     # test rgb, using rgb in Willow River
-    # img_idx = 0
-    # image_path = os.path.expanduser('~/Data/Arctic/canada_arctic/Willow_River/Planet2020/20200818_mosaic_8bit_rgb.tif')
-    # img_save_dir = os.path.join('predict_output','I%d' % img_idx)
-    # io_function.mkdir(img_save_dir)
-    # inf_list_file = os.path.join('predict_output','%d.txt'%img_idx)
-    # gpuid = None
-    # trained_model = 'exp18/latest.pth'
-    # predict_one_image_mmseg(para_file, image_path, img_save_dir, inf_list_file, gpuid, trained_model)
-    #
-    # # curr_dir,img_idx, area_save_dir, test_id
-    # curr_dir = os.getcwd()
-    # inf_results_to_shapefile(curr_dir,img_idx,'predict_output','1')
+    img_idx = 0
+    image_path = os.path.expanduser('~/Data/Arctic/canada_arctic/Willow_River/Planet2020/20200818_mosaic_8bit_rgb.tif')
+    img_save_dir = os.path.join('predict_output','I%d' % img_idx)
+    io_function.mkdir(img_save_dir)
+    inf_list_file = os.path.join('predict_output','%d.txt'%img_idx)
+    gpuid = None
+    predict_one_image_mmseg(para_file, image_path, img_save_dir, inf_list_file, gpuid, trained_model)
+
+    # curr_dir,img_idx, area_save_dir, test_id
+    curr_dir = os.getcwd()
+    inf_results_to_shapefile(curr_dir,img_idx,'predict_output','1')
 
     ############ test nirGB, using rgb in Willow River
     img_idx = 1
@@ -64,7 +64,7 @@ def test_dataloader():
     io_function.mkdir(img_save_dir)
     inf_list_file = os.path.join('predict_output','%d.txt'%img_idx)
     gpuid = None
-    trained_model = 'exp18/latest.pth'
+
     predict_one_image_mmseg(para_file, image_path, img_save_dir, inf_list_file, gpuid, trained_model)
 
     # curr_dir,img_idx, area_save_dir, test_id
