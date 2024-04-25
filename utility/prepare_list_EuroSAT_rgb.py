6710dcd1c96e28dc8839aee3c46549f2a4b824f6
Lingcao Huang <huanglingcao@gmail.com>
2024-04-25 10:22:04 +0800

    bug fix

--- utility/prepare_list_EuroSAT_rgb.py
+++ utility/prepare_list_EuroSAT_rgb.py
@@ -4,8 +4,12 @@ all_label = []
 
 data_root = os.path.expanduser('~/Data/image_classification/EuroSAT')
 
-all_classes = os.listdir(data_root + 'EuroSAT_RGB')
+all_classes = os.listdir(os.path.join(data_root, 'EuroSAT_RGB'))
+# on Mac, there is a hidden file
+if '.DS_Store' in all_classes:
+    all_classes.remove('.DS_Store')
 print(all_classes)
+
 
 f1 = open(os.path.join(data_root, 'EuroSAT_RGB_image_vs_label.txt'), 'w')
 f2 = open(os.path.join(data_root, 'EuroSAT_label_list.txt'), 'w')
@@ -14,7 +18,8 @@ for i, cls in enumerate(all_classes):
     print(cls, i)
     f2.writelines(cls + ', ' + str(i) + '\n')
 
-    cls_list = glob.glob(os.path.join(data_root, cls, '*.jpg'))
+    cls_list = glob.glob(os.path.join(data_root, 'EuroSAT_RGB' , cls, '*.jpg'))
+    # print(cls_list)
     cls_list = ["/".join(dd.split('/')[-2:]) for dd in cls_list]
 
     for cc in cls_list:
