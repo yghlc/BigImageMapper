18fba84c516e7dffe2e8888a0221bd3b8195e3ea
Lingcao Huang <huanglingcao@gmail.com>
2024-04-27 09:25:30 +0800

    cancel realative path

--- img_classification/get_organize_training_data.py
+++ img_classification/get_organize_training_data.py
@@ -115,7 +115,7 @@ def read_sub_image_labels_one_region(save_img_dir, para_file, area_ini, b_traini
 
     if os.path.isfile(patch_list_txt) is False:
         # save the relative path and label to file
-        image_path_label_list = ['%s %d' % (os.path.relpath(item), idx) for idx, item in
+        image_path_label_list = ['%s %d' % (item, idx) for idx, item in
                                  zip(image_labels, image_path_list)]
         io_function.save_list_to_txt(patch_list_txt, image_path_label_list)
 
