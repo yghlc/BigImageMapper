b0b64aafef6e5d5ed945a1517f7de832d7a51578
Lingcao Huang <huanglingcao@gmail.com>
2024-04-29 09:44:52 +0800

    only select a few samples for each classes

--- img_classification/get_organize_training_data.py
+++ img_classification/get_organize_training_data.py
@@ -20,6 +20,7 @@ import basic_src.io_function as io_function
 import basic_src.timeTools as timeTools
 
 import time
+import random
 
 import class_utils
 
@@ -64,6 +65,62 @@ def read_label_ids_local(label_txt):
         label_ids[tmp[0].lower()] = int(tmp[1]) # change to lower case
     return label_ids
 
+
+def randomly_select_k_samples_each_classes(image_labels_txt, save_path, sample_count=10):
+    '''
+    randonly select a few samples (k) for each class
+    :param image_labels_txt:  all samples, each line contain: image_path class_id
+    :param save_path:  save path
+    :param sample_count: the count of samples want to select
+    :return:
+    '''
+    image_path_list, image_labels = read_image_path_label_from_txt(image_labels_txt)
+    # group classes
+    class_id_images = {}
+    for img, label in zip(image_path_list, image_labels):
+        class_id_images.setdefault(label, []).append(img)
+
+    sel_class_id_images = {}
+    not_sel_class_id_images = {}
+    # the number of sample for each class
+    for key in class_id_images.keys():
+        print('the sample count of class %d is %d'%(key, len(class_id_images[key])))
+        if sample_count > len(class_id_images[key]):
+            raise ValueError('for class %d, select sample count (%d) is larger than the total number (%d) of all samples'%(key, sample_count,len(class_id_images[key])))
+        sel_class_id_images[key] = random.sample(class_id_images[key], sample_count)
+        # save those not selected ones (no duplicated elements)
+        not_sel_class_id_images[key] = list(set(class_id_images[key]) - set(sel_class_id_images[key]))
+
+    # save to path
+    image_path_list_sel = []
+    image_labels_sel = []
+    for key in sel_class_id_images.keys():
+        image_path_list_sel.extend(sel_class_id_images[key])
+        image_labels_sel.extend([key]*len(sel_class_id_images[key]))
+    save_image_path_label_to_txt(image_path_list_sel, image_labels_sel, save_path)
+
+    save_path_not_sel = io_function.get_name_by_adding_tail(save_path,'notSel')
+    image_path_list_not_sel = []
+    image_labels_not_sel = []
+    for key in sel_class_id_images.keys():
+        image_path_list_not_sel.extend(not_sel_class_id_images[key])
+        image_labels_not_sel.extend([key]*len(not_sel_class_id_images[key]))
+    save_image_path_label_to_txt(image_path_list_not_sel, image_labels_not_sel, save_path_not_sel)
+
+
+
+def read_image_path_label_from_txt(patch_list_txt):
+    image_path_labels = [item.split() for item in io_function.read_list_from_txt(patch_list_txt)]
+    image_path_list = [item[0] for item in image_path_labels]
+    image_labels = [int(item[1]) for item in image_path_labels]
+    return image_path_list, image_labels
+
+def save_image_path_label_to_txt(image_path_list, image_labels, patch_list_txt):
+    image_path_label_list = ['%s %d' % (item, idx) for idx, item in
+                             zip(image_labels, image_path_list)]
+    io_function.save_list_to_txt(patch_list_txt, image_path_label_list)
+
+
 def read_sub_image_labels_one_region(save_img_dir, para_file, area_ini, b_training=True):
     '''
     read image patches and labels (if available) from one region for image classification (no need to extract from big imagery using vector file)
@@ -78,9 +135,7 @@ def read_sub_image_labels_one_region(save_img_dir, para_file, area_ini, b_traini
     patch_list_txt = os.path.join(save_img_dir, area_name_remark_time + '_patch_list.txt')
     if os.path.isfile(patch_list_txt):
         print('%s exists, read it directly'%patch_list_txt)
-        image_path_labels = [item.split() for item in io_function.read_list_from_txt(patch_list_txt)]
-        image_path_list = [item[0] for item in image_path_labels]
-        image_labels = [int(item[1]) for item in image_path_labels]
+        image_path_list, image_labels = read_image_path_label_from_txt(patch_list_txt)
         return image_path_list, image_labels, patch_list_txt
 
     # class ids for this specific regions
@@ -115,9 +170,8 @@ def read_sub_image_labels_one_region(save_img_dir, para_file, area_ini, b_traini
 
     if os.path.isfile(patch_list_txt) is False:
         # save the relative path and label to file
-        image_path_label_list = ['%s %d' % (item, idx) for idx, item in
-                                 zip(image_labels, image_path_list)]
-        io_function.save_list_to_txt(patch_list_txt, image_path_label_list)
+        save_image_path_label_to_txt(image_labels, image_path_list, patch_list_txt)
+
 
     return image_path_list, image_labels, patch_list_txt
 
@@ -272,6 +326,13 @@ def get_sub_images_multi_regions_for_training(WORK_DIR, para_file):
     save_path = class_utils.get_merged_training_data_txt(training_data_dir, expr_name, len(training_regions))
     merge_imagePatch_labels_for_multi_regions(image_patch_labels_list_txts, save_path)
 
+    a_few_shot_samp_count = parameters.get_digit_parameters_None_if_absence(para_file,'a_few_shot_samp_count','int')
+    if a_few_shot_samp_count is not None:
+        # backup the original file
+        save_path_all_samp = io_function.get_name_by_adding_tail(save_path,'all')
+        io_function.copy_file_to_dst(save_path,save_path_all_samp)
+        randomly_select_k_samples_each_classes(save_path_all_samp, save_path,sample_count=a_few_shot_samp_count)
+
 
     duration= time.time() - SECONDS
     os.system('echo "$(date): time cost of getting training data (image classification): %.2f seconds">>time_cost.txt'%duration)
@@ -288,7 +349,7 @@ def main(options, args):
 if __name__ == '__main__':
 
     usage = "usage: %prog [options] para_file "
-    parser = OptionParser(usage=usage, version="1.0 2021-01-19")
+    parser = OptionParser(usage=usage, version="1.0 2024-04-26")
     parser.description = 'Introduction: extract sub-images and sub-labels '
 
     (options, args) = parser.parse_args()
