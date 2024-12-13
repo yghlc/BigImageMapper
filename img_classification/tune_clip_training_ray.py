600b44d0b8730704be4f8367f3e42a7b97ec0e83
Lingcao Huang <hlchuang@126.com>
2024-12-13 09:02:19 +0800

    bug fix

--- img_classification/tune_clip_training_ray.py
+++ img_classification/tune_clip_training_ray.py
@@ -227,7 +227,7 @@ def main():
                                                                           "This is a DEM hillshade of a {}."]
     # a_few_shot_samp_count
     a_few_shot_samp_count_list = [10, 50, 100, 200, 300, 600, 1000] #
-    model_type_list = ['RN50', 'RN101', 'RN50x4', 'RN50x16', 'ViT-B/32', 'ViT-B/16', 'ViT-L/14', 'ViT-L-14-336px'] #
+    model_type_list = ['RN50', 'RN101', 'RN50x4', 'RN50x16', 'ViT-B/32', 'ViT-B/16', 'ViT-L/14', 'ViT-L/14@336px'] #
     # due to the setting in train_clip.py, train_epoch_num must be >= 100, train_epoch_num%100 =0
     train_epoch_num_list = [100, 200, 300, 500]  #
     base_learning_rate_list = [1e-5, 1e-4, 5e-5]  #
