a5f5d5e2c859e7cbf93ad2ce86e26f9dc19d4da3
Lingcao Huang <hlchuang@126.com>
2024-12-20 17:38:08 +0800

    run again

--- img_classification/tune_clip_training_ray.py
+++ img_classification/tune_clip_training_ray.py
@@ -270,14 +270,14 @@ def main():
     train_epoch_num_list = [100, 200,  300, 500]  #
     base_learning_rate_list = [1e-5, 1e-4, 5e-5]  #
 
-    # debugging
-    a_few_shot_samp_count_list = [ 300] #
-    # 'ViT-L/14@336px': on ygAlpha (4090D), not enough CUDA memory
-    #  'RN50x16': on ygAlpha (4090D), not enough CUDA memory after 100 epochs
-    model_type_list = ['RN50', 'RN101', 'RN50x4', 'ViT-B/32', 'ViT-B/16', 'ViT-L/14'] # ,
-    # due to the setting in train_clip.py, train_epoch_num must be >= 100, train_epoch_num%100 =0
-    train_epoch_num_list = [300]  #
-    base_learning_rate_list = [1e-5]  #
+    # # debugging
+    # a_few_shot_samp_count_list = [ 300] #
+    # # 'ViT-L/14@336px': on ygAlpha (4090D), not enough CUDA memory
+    # #  'RN50x16': on ygAlpha (4090D), not enough CUDA memory after 100 epochs
+    # model_type_list = ['RN50', 'RN101', 'RN50x4', 'ViT-B/32', 'ViT-B/16', 'ViT-L/14'] # ,
+    # # due to the setting in train_clip.py, train_epoch_num must be >= 100, train_epoch_num%100 =0
+    # train_epoch_num_list = [300]  #
+    # base_learning_rate_list = [1e-5]  #
 
     # Check if there are existing folders in the tuning directory
     file_folders = io_function.get_file_list_by_pattern(os.path.join(loc_dir, tune_name), '*')
