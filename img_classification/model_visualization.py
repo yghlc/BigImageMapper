eb80e49121c0199d7e9a660e35cdd02b8e2a8b75
Lingcao Huang <yghlc@users.noreply.github.com>
2025-06-25 11:39:57 +0800

    CLIP GradCAM Visualization

--- img_classification/model_visualization.py
+++ img_classification/model_visualization.py
@@ -421,8 +421,159 @@ def test_tSNE_CLIP_visualization(device):
     # t-SNE visualization of features extracted by the visual encoder network from the UCM
     tSNE_CLIP_UCM_dataset(device)
 
+##########################################################################################
+# adopt from: https://colab.research.google.com/github/kevinzakka/clip_playground/blob/main/CLIP_GradCAM_Visualization.ipynb
 
-    
+from scipy.ndimage import filters
+from torch import nn
+import torch.nn.functional as F
+
+def normalize(x: np.ndarray) -> np.ndarray:
+    # Normalize to [0, 1].
+    x = x - x.min()
+    if x.max() > 0:
+        x = x / x.max()
+    return x
+
+
+# Modified from: https://github.com/salesforce/ALBEF/blob/main/visualization.ipynb
+def getAttMap(img, attn_map, blur=True):
+    if blur:
+        attn_map = filters.gaussian_filter(attn_map, 0.02 * max(img.shape[:2]))
+    attn_map = normalize(attn_map)
+    cmap = plt.get_cmap('jet')
+    attn_map_c = np.delete(cmap(attn_map), 3, 2)
+    attn_map = 1 * (1 - attn_map ** 0.7).reshape(attn_map.shape + (1,)) * img + \
+               (attn_map ** 0.7).reshape(attn_map.shape + (1,)) * attn_map_c
+    return attn_map
+
+
+def viz_attn(img, attn_map, blur=True, save_path='heatmap.jpg'):
+    _, axes = plt.subplots(1, 2, figsize=(10, 5))
+    axes[0].imshow(img)
+    axes[1].imshow(getAttMap(img, attn_map, blur))
+    for ax in axes:
+        ax.axis("off")
+    plt.savefig(save_path,dpi=200)
+    plt.show()
+
+
+def load_image(img_path, resize=None):
+    image = Image.open(img_path).convert("RGB")
+    if resize is not None:
+        image = image.resize((resize, resize))
+    return np.asarray(image).astype(np.float32) / 255.
+
+
+class Hook:
+    """Attaches to a module and records its activations and gradients."""
+
+    def __init__(self, module: nn.Module):
+        self.data = None
+        self.hook = module.register_forward_hook(self.save_grad)
+
+    def save_grad(self, module, input, output):
+        self.data = output
+        output.requires_grad_(True)
+        output.retain_grad()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.hook.remove()
+
+    @property
+    def activation(self) -> torch.Tensor:
+        return self.data
+
+    @property
+    def gradient(self) -> torch.Tensor:
+        return self.data.grad
+
+
+# Reference: https://arxiv.org/abs/1610.02391
+def gradCAM(
+        model: nn.Module,
+        input: torch.Tensor,
+        target: torch.Tensor,
+        layer: nn.Module
+) -> torch.Tensor:
+    # Zero out any gradients at the input.
+    if input.grad is not None:
+        input.grad.data.zero_()
+
+    # Disable gradient settings.
+    requires_grad = {}
+    for name, param in model.named_parameters():
+        requires_grad[name] = param.requires_grad
+        param.requires_grad_(False)
+
+    # Attach a hook to the model at the desired layer.
+    assert isinstance(layer, nn.Module)
+    with Hook(layer) as hook:
+        # Do a forward and backward pass.
+        output = model(input)
+        output.backward(target)
+
+        grad = hook.gradient.float()
+        act = hook.activation.float()
+
+        # Global average pool gradient across spatial dimension
+        # to obtain importance weights.
+        alpha = grad.mean(dim=(2, 3), keepdim=True)
+        # Weighted combination of activation maps over channel
+        # dimension.
+        gradcam = torch.sum(act * alpha, dim=1, keepdim=True)
+        # We only want neurons with positive influence so we
+        # clamp any negative ones.
+        gradcam = torch.clamp(gradcam, min=0)
+
+    # Resize gradcam to input resolution.
+    gradcam = F.interpolate(
+        gradcam,
+        input.shape[2:],
+        mode='bicubic',
+        align_corners=False)
+
+    # Restore gradient settings.
+    for name, param in model.named_parameters():
+        param.requires_grad_(requires_grad[name])
+
+    return gradcam
+
+def CLIP_GradCAM_Visualization(image_path, image_caption, clip_model='RN50', saliency_layer='layer4',save_fig='heatmap.jpg'):
+    image_caption = image_caption  # @param {type:"string"}
+    # @markdown ---
+    # @markdown #### CLIP model settings
+    clip_model = clip_model  # @param ["RN50", "RN101", "RN50x4", "RN50x16"]
+    saliency_layer = saliency_layer  # @param ["layer4", "layer3", "layer2", "layer1"]
+    # @markdown ---
+    # @markdown #### Visualization settings
+    blur = True  # @param {type:"boolean"}
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, preprocess = clip.load(clip_model, device=device, jit=False)
+
+    # Download the image from the web.
+    # image_path = 'n01622779_great_grey_owl.JPEG'
+    # urllib.request.urlretrieve(image_url, image_path)
+
+    image_input = preprocess(Image.open(image_path)).unsqueeze(0).to(device)
+    image_np = load_image(image_path, model.visual.input_resolution)
+    text_input = clip.tokenize([image_caption]).to(device)
+
+    attn_map = gradCAM(
+        model.visual,
+        image_input,
+        model.encode_text(text_input).float(),
+        getattr(model.visual, saliency_layer)
+    )
+    attn_map = attn_map.squeeze().detach().cpu().numpy()
+
+    viz_attn(image_np, attn_map, blur, save_path=save_fig)
+
+##########################################################################################
 
 
 def heatmap_clip_classification(WORK_DIR, para_file, device, img_path, trained_model=None, save_fig=None):
@@ -452,10 +603,29 @@ def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     # test_heatmap_clip_one_figure()
+    # image_path = 'n01622779_great_grey_owl.jpeg'
+    # image_caption = "the owl"
+
+    # image_path = 'n02123159_tiger_cat.JPEG'
+    # image_caption = "the tiger cat"
+
+    # image_path = 'a_cat.png'
+    # image_caption = "a cat"
+
+    # image_path = 'Screenshot_slump_canada.png'
+    # image_caption = "satellite image of a landslide"
+
+    image_path = 'intersection88.tif'
+    image_caption = 'intersection'
+    image_caption = 'the satellite image of intersection'
+
+    save_fig = f'heatmap_{image_caption}.jpg'
+    CLIP_GradCAM_Visualization(image_path, image_caption, clip_model='RN50', saliency_layer='layer4',save_fig=save_fig)
+
     
     # tSNE_visualiztion()
     # test_tSNE_CLIP_visualization(device)
-    tSNE_CLIP_S2_slump_images(device)
+    # tSNE_CLIP_S2_slump_images(device)
 
     pass
 
