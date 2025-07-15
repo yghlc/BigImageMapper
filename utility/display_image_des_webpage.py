c9dc8d5da562299cd38a602ebd94836ab1ab05da
yghlc <hlchuang@126.com>
2025-07-15 16:44:13 +0800

    add file name

--- utility/display_image_des_webpage.py
+++ utility/display_image_des_webpage.py
@@ -85,11 +85,12 @@ def display_images_values_s2(image_value_json, rgb_bands=[1,2,3], img_dir=None, 
             img_path_2 = img_path
 
         # for sentinel-2 images
+        #TODO: change to visualizton to 0-2000 ? for sentinel-2 images?
         raster_io.convert_images_to_rgb_8bit_np(img_path_2,save_path=png_path,rgb_bands=rgb_bands,sr_min=0,sr_max=1600,
                                                 nodata=0,format='PNG',verbose=False)
         valid_percent = img_value_dict[img_path][0]
         entropy = img_value_dict[img_path][1]
-        save_dict[png_path] = f"v_perc={valid_percent:.1f}%, entropy={entropy:.3f}" # str(img_value_dict[img_path])
+        save_dict[png_path] = f"{os.path.basename(img_path_2)}\nv_perc={valid_percent:.1f}%, entropy={entropy:.3f}" # str(img_value_dict[img_path])
 
     io_function.save_dict_to_txt_json(save_json, save_dict)
 
