4552b12c02e72d6a6a74776a087bb79bfdb5c503
Lingcao Huang <yghlc@users.noreply.github.com>
2025-08-03 11:11:07 +0800

    select isolated files before dividing into groups

--- datasets/geo_index_h3.py
+++ datasets/geo_index_h3.py
@@ -14,7 +14,9 @@ add time: 24 July, 2025
 import os
 import h3
 import geopandas as gpd
+from collections import Counter
 
+import basic_src.io_function as io_function
 
 def short_h3(h3id):
     return h3id.rstrip('f')
@@ -119,10 +121,114 @@ def test_test_get_h3_cell_id():
 
 
 
+def select_isolated_files_h3(h3_id_list, threshold_grid_k):
+    """
+    Finds isolated H3 IDs: for each, if the total count of itself and all its
+    neighbors (within threshold_grid_k) is 1, it's considered isolated.
+    """
+    cell_count = Counter(h3_id_list)
+    io_function.save_dict_to_txt_json('cell_count.txt',cell_count)
+    isolated = []
+    isolated_idx = []
+
+
+    for idx, h3_id in enumerate(h3_id_list):
+        neighborhood = h3.grid_disk(h3_id, threshold_grid_k)
+        # cell_count is a Counter object, which is a subclass of dict.
+        # If you ask for a key (an H3 ID) that does not exist in the Counter,
+        # it returns 0 instead of raising a KeyError.
+        total_count = sum(cell_count[neighbor] for neighbor in neighborhood)
+        if total_count == 1:
+            isolated.append(h3_id)
+            isolated_idx.append(idx)
+    return isolated, isolated_idx
+
+def select_isolated_files_h3_at_res(h3_id_list, threshold_grid_k, res):
+    """
+    Converts all H3 IDs to the specified resolution (keeping duplicates),
+    then finds isolated IDs at that resolution.
+
+    Args:
+        h3_id_list (list): List of H3 indexes (strings).
+        threshold_grid_k (int): Neighborhood k for isolation.
+        res (int): Target H3 resolution.
+
+    Returns:
+        list: Isolated H3 IDs at the specified resolution (duplicates preserved).
+    """
+    converted_ids = []
+    for h in h3_id_list:
+        # print(h)
+        if not h3.is_valid_cell(h):
+            raise ValueError(f'cell: {h} is invalid')
+        curr_res = h3.get_resolution(h)
+
+        if curr_res > res:
+            converted_ids.append(h3.cell_to_parent(h, res))
+        elif curr_res == res:
+            converted_ids.append(h)
+        # If curr_res < res, ignore (or handle as needed for your use case)
+        else:
+            raise ValueError(f'current res: {curr_res} is smaller than targeted res: {res}')
+
+        # print(curr_res, res)
+    # print(converted_ids[0],converted_ids[1])
+    # print('debugging, h3_id_list, threshold_grid_k, res', len(h3_id_list), threshold_grid_k, res)
+    # print('debugging, converted_ids', len(converted_ids))
+    c_isolate_ids, c_isolated_idx = select_isolated_files_h3(converted_ids, threshold_grid_k)
+
+    # get the IDs  at the original res
+    sel_ids = [h3_id_list[idx] for idx in c_isolated_idx]
+
+    return sel_ids, c_isolated_idx
+
+
+def test_select_isolated_files_h3_at_res():
+    data_dir = os.path.expanduser('~/Data/rts_ArcticDEM_mapping/extract_sub_images')
+    # txt = os.path.join(data_dir,'sub_images_labels_list.txt')
+    txt = os.path.join(data_dir,'sub_images_labels_list_10000.txt')
+    h3_ids_list = [os.path.splitext(os.path.basename(item))[0] for item in  io_function.read_list_from_txt(txt)]
+    h3_ids_list = [item.split('_')[0]  for item in h3_ids_list] # for case like: 8e05100e9b8e317_1, remove "_1"
+    # in default, we use shorted function
+    h3_ids_list = [unshort_h3(item) for item in h3_ids_list]
+    print(h3_ids_list[0], h3_ids_list[1], h3_ids_list[2])
+
+    threshold_grid_k = 1
+    res = 8
+    # res = 14
+    isolated_ids, _ = select_isolated_files_h3_at_res(h3_ids_list, threshold_grid_k, res)
+    io_function.save_list_to_txt('isolated_ids.txt',isolated_ids)
+
+
+def get_grid_distance_between_cells(cell1, cell2):
+    if h3.get_resolution(cell1) != h3.get_resolution(cell2):
+        raise ValueError("Cells are not at the same resolution!")
+
+    if not h3.is_valid_cell(cell1) or not h3.is_valid_cell(cell2):
+        raise ValueError("Invalid H3 index!")
+
+    # Calculate the grid distance
+    distance = h3.grid_distance(cell1, cell2)
+    print(f"The H3 grid distance between the two cells is: {distance}")
+
+def test_get_grid_distance_between_cells():
+    # Define two H3 cell IDs at the same resolution
+    lat1, lon1 = 40.7128, -74.0060
+    lat2, lon2 = 34.0522, -74.2437
+    # h3_cell1 = h3.geo_to_h3(, 9)  # New York City
+    # h3_cell2 = h3.geo_to_h3(, 9)  # Los Angeles
+
+    cell1 = get_h3_cell_id(lat1,lon1,9)
+    cell2 = get_h3_cell_id(lat2,lon2,9)
+
+    get_grid_distance_between_cells(cell1, cell2)
+
 
 def main():
     # test_get_h3_cell_id()
-    test_test_get_h3_cell_id()
+    # test_test_get_h3_cell_id()
+    # test_get_grid_distance_between_cells()
+    test_select_isolated_files_h3_at_res()
     pass
 
 
--- utility/select_divide_file_into_groups.py
+++ utility/select_divide_file_into_groups.py
@@ -14,6 +14,7 @@ from optparse import OptionParser
 
 sys.path.insert(0, os.path.expanduser('~/codes/PycharmProjects/DeeplabforRS'))
 import basic_src.io_function as io_function
+import geo_index_h3
 
 import random
 
@@ -57,15 +58,39 @@ def organize_files_to_diff_folders(file_groups, save_dir='./random_groups', b_co
             move_files(group,g_folder,g_id_str)
 
 
+def select_spatial_isolated_files(file_list, res_h3, thr_grid_dis):
+    # to select files are there are no neighbours within a distance (thr_grid_dis)  as a h3 res (res_h3)
+    h3_ids_list = [os.path.splitext(os.path.basename(item))[0] for item in  file_list]
+    h3_ids_list = [item.split('_')[0]  for item in h3_ids_list] # for case like: 8e05100e9b8e317_1, remove "_1"
+    # in default, we use shorted function
+    h3_ids_list = [geo_index_h3.unshort_h3(item) for item in h3_ids_list]
+
+    sel_ids, idx_list = geo_index_h3.select_isolated_files_h3_at_res(h3_ids_list, thr_grid_dis,res_h3 )
+
+    sel_file_list = [file_list[item] for item in idx_list]
+    print(f'Select {len(sel_file_list)} spatial isolated files from {len(file_list)} files')
+
+    return sel_file_list
+
 def main(options, args):
     data_folder = args[0]
     file_ext = options.file_ext
     num_each_group = options.num_each_group
-
-    file_list = io_function.get_file_list_by_ext(file_ext,data_folder,bsub_folder=True)
-    file_groups = random_divide_to_groups(file_list,num_each_group=num_each_group)
+    res_isolated_h3 = options.isolated_at_h3_res
+    thr_neighbour_grid = options.neighbour_grid_k
 
     out_dir = data_folder+'_groups'
+    if os.path.exists(out_dir):
+        print(f'{out_dir} exists, please remove it if want to generate a new one')
+        return
+
+    file_list = io_function.get_file_list_by_ext(file_ext,data_folder,bsub_folder=True)
+
+    if res_isolated_h3 is not None:
+        file_list = select_spatial_isolated_files(file_list,res_isolated_h3,thr_neighbour_grid)
+
+    file_groups = random_divide_to_groups(file_list,num_each_group=num_each_group)
+
     organize_files_to_diff_folders(file_groups, save_dir=out_dir, b_copy=True)
 
 
@@ -82,6 +107,16 @@ if __name__ == '__main__':
     parser.add_option("-n", "--num_each_group",
                       action="store", dest="num_each_group", type=int, default=100,
                       help="the number of sample for selection in each group")
+
+    parser.add_option("-r", "--isolated_at_h3_res",
+                      action="store", dest="isolated_at_h3_res", type=int,
+                      help="the h3 resolution to check if the file is isolated, default is None, then don't apply this")
+
+    parser.add_option("-k", "--neighbour_grid_k",
+                      action="store", dest="neighbour_grid_k", type=int, default=1,
+                      help="if isolated_at_h3_res is set, k is the threshold for neighbouring ")
+
+
 
 
     (options, args) = parser.parse_args()
