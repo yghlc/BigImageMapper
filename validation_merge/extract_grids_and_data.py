2fe193fd8217bc329e740df8cd5f53dd01803519
Lingcao Huang <yghlc@users.noreply.github.com>
2025-09-16 12:32:50 +0800

    check the existence of json file

--- validation_merge/extract_grids_and_data.py
+++ validation_merge/extract_grids_and_data.py
@@ -170,6 +170,9 @@ def convert_tif_to_png(tif, save_path, set_name):
 
 def convert_geojson_to_pixel_json(geojson, pixel_json, set_name, ref_image):
     pixel_json = pixel_json.replace('geojson','json')
+    if os.path.isfile(pixel_json):
+        print(f'Warning, {pixel_json} exists, skip')
+        return
     polys = vector_gpd.read_polygons_gpd(geojson,b_fix_invalid_polygon=False)
     img_transform = raster_io.get_transform_from_file(ref_image)
 
