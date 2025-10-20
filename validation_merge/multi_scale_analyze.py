f54d82c6b46a45635d787a3122ebe75b4c25a0a3
Lingcao Huang <yghlc@users.noreply.github.com>
2025-10-20 09:16:39 +0800

    first calculation

--- validation_merge/multi_scale_analyze.py
+++ validation_merge/multi_scale_analyze.py
@@ -59,7 +59,9 @@ def convert_h3_cells_to_lower_scale(in_h3_cells,input_res,lower_res, lower_h3_ce
     for att in attribute_name_list:
         values = merge_attributes(in_h3_cells,att,parent_child_idx)
         add_attributes[att] = values
-    vector_gpd.add_attributes_to_shp(lower_h3_cells, add_attributes)
+
+    save_format = vector_gpd.guess_file_format_extension(lower_h3_cells)
+    vector_gpd.add_attributes_to_shp(lower_h3_cells, add_attributes,format=save_format)
 
 
 
@@ -69,12 +71,13 @@ def test_convert_h3_cells_to_lower_scale():
     data_dir = os.path.expanduser('~/Data/rts_ArcticDEM_mapping/combine_mapping_results')
     child_h3_cells_vector = os.path.join(data_dir,'ygAlpha_res','h3_cells_select_by_rf_0.9.gpkg')
     child_res = 8
-    parent_res = 4
+    parent_res = 3
     # parent_h3_cells_vector = os.path.join(data_dir,'h3_cells_for_merging_res4',
     #                     'h3_cells_res4_panArctic_s2_rgb_2024_object_detection_s2_exp5_post_1.shp')
-    parent_h3_cells_vector = os.path.join(data_dir,'h3_cells_for_merging_res4',
-    'h3_cells_res4_panArctic_composited_image_2008-17_object_detection_comImg_exp3t2_post_1.shp')
+    # parent_h3_cells_vector = os.path.join(data_dir,'h3_cells_for_merging_res4',
+    # 'h3_cells_res4_panArctic_composited_image_2008-17_object_detection_comImg_exp3t2_post_1.shp')
     # parent_h3_cells_vector = os.path.join(data_dir,'h3_cells_for_merging_res4','for_debuging.shp')
+    parent_h3_cells_vector = os.path.join(data_dir, 'h3_cells_panArctic_rts_res3.gpkg')
 
     # for_debuging.shp
 
